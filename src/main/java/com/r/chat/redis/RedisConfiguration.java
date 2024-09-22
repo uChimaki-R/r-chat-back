68292d5959fec2e4374a6303b33e01913f30d085
Crestimes <1259864733@qq.com>
2024-09-22 15:53:43 +0800

    feat: 新增redis配置类，配置redis数据序列化的方式，返回redisTemplate bean对象

---
+++ src/main/java/com/r/chat/redis/RedisConfiguration.java
@@ -0,0 +1,23 @@
+package com.r.chat.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfiguration<V> {
+    @Bean
+    public RedisTemplate<String, V> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, V> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        // key和value的序列化方式
+        template.setKeySerializer(RedisSerializer.string());
+        template.setValueSerializer(RedisSerializer.json());
+        // hash的key和value的序列化方式
+        template.setHashKeySerializer(RedisSerializer.string());
+        template.setHashValueSerializer(RedisSerializer.json());
+        return template;
+    }
+}
