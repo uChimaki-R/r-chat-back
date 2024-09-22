367b3efbd7fd633719ca2a7f7d239672dae942e2
Crestimes <1259864733@qq.com>
2024-09-22 14:54:45 +0800

    feat: 启用任务管理、事务管理、异步功能，标记mapper扫描包

--- src/main/java/com/r/chat/RChatApplication.java
+++ src/main/java/com/r/chat/RChatApplication.java
@@ -1,10 +1,17 @@
 package com.r.chat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(scanBasePackages = {"com.r.chat"}, exclude = DataSourceAutoConfiguration.class)
+@EnableScheduling
+@EnableTransactionManagement
+@EnableAsync
+@MapperScan(basePackages = {"com.r.chat.mapper"})
+@SpringBootApplication(scanBasePackages = {"com.r.chat"})
 public class RChatApplication {
     public static void main(String[] args) {
         SpringApplication.run(RChatApplication.class, args);
