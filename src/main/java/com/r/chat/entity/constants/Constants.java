90b08e4016ad6f1e112c7350655a25df2fcd154b
Crestimes <1259864733@qq.com>
2024-09-23 15:56:27 +0800

    refactor: 重命名[REDIS_KEY_CHECK_CODE->REDIS_KEY_CHECK_CODE_PREFIX]

--- src/main/java/com/r/chat/entity/constants/Constants.java
+++ src/main/java/com/r/chat/entity/constants/Constants.java
@@ -1,5 +1,5 @@
 package com.r.chat.entity.constants;
 
 public class Constants {
-    public static final String REDIS_KEY_CHECK_CODE = "rchat:checkcode:"; // redis验证码结果保存key
+    public static final String REDIS_KEY_CHECK_CODE_PREFIX = "rchat:checkcode:"; // redis验证码结果保存key
 }
