9ce223d44653417932c36e282f54a0733a1b6d78
Crestimes <1259864733@qq.com>
2024-09-23 15:58:32 +0800

    perf: 优化部分代码逻辑

--- src/main/java/com/r/chat/entity/vo/Result.java
+++ src/main/java/com/r/chat/entity/vo/Result.java
@@ -1,5 +1,6 @@
 package com.r.chat.entity.vo;
 
+import com.r.chat.entity.enums.ResponseCodeEnum;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -19,25 +20,22 @@ public class Result<T> implements Serializable {
 
     public static <T> Result<T> success() {
         Result<T> result = new Result<>();
-        result.code = 200;
-        result.status = "success";
+        result.code = ResponseCodeEnum.SUCCESS.getCode();
+        result.status = ResponseCodeEnum.SUCCESS.getMessage();
         result.info = "请求成功";
         return result;
     }
 
     public static <T> Result<T> success(T object) {
-        Result<T> result = new Result<>();
+        Result<T> result = success();
         result.data = object;
-        result.code = 200;
-        result.status = "success";
-        result.info = "请求成功";
         return result;
     }
 
-    public static <T> Result<T> error(String msg) {
+    public static <T> Result<T> error(Integer code, String msg) {
         Result<T> result = new Result<>();
-        result.code = 0;
-        result.status = "error";
+        result.code = code;
+        result.status = "Error";
         result.info = msg;
         return result;
     }
