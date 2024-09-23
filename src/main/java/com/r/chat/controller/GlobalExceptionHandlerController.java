51021698467c3018ac3bec7609e4dd72335f809b
Crestimes <1259864733@qq.com>
2024-09-23 17:50:44 +0800

    perf: 优化全局异常处理controller的代码逻辑

--- src/main/java/com/r/chat/controller/GlobalExceptionHandlerController.java
+++ src/main/java/com/r/chat/controller/GlobalExceptionHandlerController.java
@@ -3,17 +3,25 @@ package com.r.chat.controller;
 import com.r.chat.entity.enums.ResponseCodeEnum;
 import com.r.chat.entity.vo.Result;
 import com.r.chat.exception.BusinessException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-@RestControllerAdvice()
+import java.net.BindException;
+
+@RestControllerAdvice
 public class GlobalExceptionHandlerController {
     @ExceptionHandler(Exception.class)
     public Result<String> handleException(Exception e) {
         if (e instanceof NoHandlerFoundException) {
             // 404
             return Result.error(ResponseCodeEnum.NOT_FOUND.getCode(), ResponseCodeEnum.NOT_FOUND.getMessage());
+        }
+        else if (e instanceof BindException || e instanceof MethodArgumentNotValidException || e instanceof HttpRequestMethodNotSupportedException) {
+            // 拒接请求
+            return Result.error(ResponseCodeEnum.NOT_ACCEPTABLE.getCode(), ResponseCodeEnum.NOT_ACCEPTABLE.getMessage());
         }
         else if (e instanceof BusinessException) {
             // 业务异常
