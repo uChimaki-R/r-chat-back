f50654aabb7ed3cd569790d3cc5d06b356e8e615
Crestimes <1259864733@qq.com>
2024-09-23 17:46:50 +0800

    feat: 新增枚举ID_LENGTH代指用户id及群组id的长度

--- src/main/java/com/r/chat/entity/constants/Constants.java
+++ src/main/java/com/r/chat/entity/constants/Constants.java
@@ -2,4 +2,6 @@ package com.r.chat.entity.constants;
 
 public class Constants {
     public static final String REDIS_KEY_CHECK_CODE_PREFIX = "rchat:checkcode:"; // redis验证码结果保存key
+
+    public static final Integer ID_LENGTH = 11; // 用户id及群组id的长度
 }
