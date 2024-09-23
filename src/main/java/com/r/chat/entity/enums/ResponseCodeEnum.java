749076d8db3a71258286bb385120da525fbe0dc0
Crestimes <1259864733@qq.com>
2024-09-23 15:57:14 +0800

    feat: 新增响应信息枚举类

---
+++ src/main/java/com/r/chat/entity/enums/ResponseCodeEnum.java
@@ -0,0 +1,22 @@
+package com.r.chat.entity.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum ResponseCodeEnum {
+    SUCCESS(200, "Success"),
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int code;
+    private final String message;
+
+    ResponseCodeEnum(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+}
