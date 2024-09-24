3a7e5b524288356c571b251421ef895d663ac711
Crestimes <1259864733@qq.com>
2024-09-24 14:18:39 +0800

    fix: 修正部分代码

--- src/main/java/com/r/chat/entity/dto/RegisterDTO.java
+++ src/main/java/com/r/chat/entity/dto/RegisterDTO.java
@@ -5,11 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class RegisterDTO {
+public class RegisterDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String checkCodeKey; // UUID唯一标识
     private String checkCode; // 发送的验证码结果
     private String email;
