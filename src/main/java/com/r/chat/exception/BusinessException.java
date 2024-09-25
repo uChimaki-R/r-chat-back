b06b51186aecba83b8032bcbb078d89f9400741d
Crestimes <1259864733@qq.com>
2024-09-25 11:05:12 +0800

    perf: 简化BusinessException类以用作父类

--- src/main/java/com/r/chat/exception/BusinessException.java
+++ src/main/java/com/r/chat/exception/BusinessException.java
@@ -1,20 +1,12 @@
 package com.r.chat.exception;
 
-import lombok.Getter;
 
-@Getter
-public class BusinessException extends BaseException {
-    private Integer code;
+public class BusinessException extends RuntimeException {
 
     public BusinessException() {
     }
 
     public BusinessException(String message) {
         super(message);
-    }
-
-    public BusinessException(String message, Integer code) {
-        super(message);
-        this.code = code;
     }
 }
