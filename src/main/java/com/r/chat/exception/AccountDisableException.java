b547ab26b2a079c3fc9d37a8a0544393eb8df7a2
Crestimes <1259864733@qq.com>
2024-09-25 12:14:35 +0800

    feat: 新增账号被锁定的异常类

---
+++ src/main/java/com/r/chat/exception/AccountDisableException.java
@@ -0,0 +1,9 @@
+package com.r.chat.exception;
+
+public class AccountDisableException extends BusinessException {
+    public AccountDisableException() {
+    }
+
+    public AccountDisableException(String message) {
+    }
+}
