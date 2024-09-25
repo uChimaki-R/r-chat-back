64edf8d91bfdcf3faee1b01bb7a62b18a7f129a1
Crestimes <1259864733@qq.com>
2024-09-25 10:44:39 +0800

    feat: 新增多个成员属性

--- src/main/java/com/r/chat/entity/vo/UserInfoVO.java
+++ src/main/java/com/r/chat/entity/vo/UserInfoVO.java
@@ -1,17 +1,27 @@
 package com.r.chat.entity.vo;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class UserInfoVO implements Serializable {
     private static final long serialVersionUID = 1L;
     private String userId;
+    private String email;
+    private String password;
     private String nickName;
-    private boolean admin = false;
+    private Integer gender;
     private String token;
+    private Integer joinType; // 好友添加模式 0: 可以直接添加好友 1: 同意后添加好友
+    private String personalSignature;
+    private String areaName;
+    private String areaCode;
+
+    private boolean admin = false;
+    private Integer contactStatus; // 当前状态
 }
