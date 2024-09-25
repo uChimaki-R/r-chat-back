1d7b714184e953471eecf6fe445f9b7cd637a4b4
Crestimes <1259864733@qq.com>
2024-09-25 10:49:21 +0800

    refactor: 修改业务异常时返回的状态码，优化代码逻辑

--- src/main/java/com/r/chat/controller/GlobalExceptionHandlerController.java
+++ src/main/java/com/r/chat/controller/GlobalExceptionHandlerController.java
@@ -18,20 +18,14 @@ public class GlobalExceptionHandlerController {
         if (e instanceof NoHandlerFoundException) {
             // 404
             return Result.error(ResponseCodeEnum.NOT_FOUND.getCode(), ResponseCodeEnum.NOT_FOUND.getMessage());
-        }
-        else if (e instanceof BindException || e instanceof MethodArgumentNotValidException || e instanceof HttpRequestMethodNotSupportedException) {
+        } else if (e instanceof BindException || e instanceof MethodArgumentNotValidException || e instanceof HttpRequestMethodNotSupportedException) {
             // 拒接请求
             return Result.error(ResponseCodeEnum.NOT_ACCEPTABLE.getCode(), ResponseCodeEnum.NOT_ACCEPTABLE.getMessage());
-        }
-        else if (e instanceof BusinessException) {
+        } else if (e instanceof BusinessException) {
             // 业务异常
             BusinessException be = (BusinessException) e;
-            if (be.getCode() != null) {
-                return Result.error(be.getCode(), be.getMessage());
-            }
-            return Result.error(ResponseCodeEnum.INTERNAL_SERVER_ERROR.getCode(), be.getMessage());
-        }
-        else {
+            return Result.error(ResponseCodeEnum.FORBIDDEN.getCode(), be.getMessage());
+        } else {
             return Result.error(ResponseCodeEnum.INTERNAL_SERVER_ERROR.getCode(), ResponseCodeEnum.INTERNAL_SERVER_ERROR.getMessage());
         }
     }
