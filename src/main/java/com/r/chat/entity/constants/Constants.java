9a2f5c4e4d60dda82b8dea5e840437c58cee32ea
Crestimes <1259864733@qq.com>
2024-09-25 10:48:21 +0800

    feat: 新增枚举：redis的ws的心跳保存前缀、用户token保存前缀、用户id保存前缀，token中拼接的随机字符串的长度，重命名[ID_LENGTH->LENGTH_ID]

--- src/main/java/com/r/chat/entity/constants/Constants.java
+++ src/main/java/com/r/chat/entity/constants/Constants.java
@@ -1,7 +1,11 @@
 package com.r.chat.entity.constants;
 
 public class Constants {
-    public static final String REDIS_KEY_CHECK_CODE_PREFIX = "rchat:checkcode:"; // redis验证码结果保存key
+    public static final String REDIS_KEY_CHECK_CODE_PREFIX = "rchat:checkcode:";  // 验证码结果保存前缀
+    public static final String REDIS_KEY_WS_HEART_BEAT_PREFIX = "rchat:ws:heartbeat:";  // ws的心跳保存前缀
+    public static final String REDIS_KEY_USER_TOKEN_PREFIX = "rchat:user:token:";  // 用户token保存前缀
+    public static final String REDIS_KEY_USER_ID_PREFIX = "rchat:user:id:";  // 用户id保存前缀
 
-    public static final Integer ID_LENGTH = 11; // 用户id及群组id的长度
+    public static final Integer LENGTH_ID = 11;  // 用户id及群组id的长度
+    public static final Integer LENGTH_TOKEN_RANDOM_CHARS = 20;  // token中拼接的随机字符串的长度
 }
