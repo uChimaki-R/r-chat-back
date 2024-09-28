720a50044636c8bed40a8dd2c116aa66bebcb5cc
Crestimes <1259864733@qq.com>
2024-09-28 20:03:39 +0800

    feat: 新增联系人申请状态枚举类

---
+++ src/main/java/com/r/chat/entity/enums/UserContactApplyStatusEnum.java
@@ -0,0 +1,21 @@
+package com.r.chat.entity.enums;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import lombok.Getter;
+
+@Getter
+public enum UserContactApplyStatusEnum {
+    // 用户联系人申请状态：0：待处理 1：已同意 2：已拒绝 3：已拉黑
+    PENDING(0, "待处理"),
+    AGREED(1, "已同意"),
+    REJECTED(2, "已拒绝"),
+    BLOCKED(3, "已拉黑");
+    @EnumValue
+    private final int value;
+    private final String desc;
+
+    UserContactApplyStatusEnum(int value, String desc) {
+        this.value = value;
+        this.desc = desc;
+    }
+}
