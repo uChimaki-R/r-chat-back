bc4788173b16c54028382795f779a4b9afd798c3
Crestimes <1259864733@qq.com>
2024-09-28 21:55:14 +0800

    refactor: 添加成员变量code，并要求子类初始化，以此在新增子类的时候指定对应的响应码，而响应信息由调用子类时使用常量传入。

--- src/main/java/com/r/chat/exception/BusinessException.java
+++ src/main/java/com/r/chat/exception/BusinessException.java
@@ -1,12 +1,14 @@
 package com.r.chat.exception;
 
+import lombok.Getter;
 
+@Getter
 public class BusinessException extends RuntimeException {
+    private final Integer code;
 
-    public BusinessException() {
-    }
-
-    public BusinessException(String message) {
+    public BusinessException(String message, Integer code) {
         super(message);
+        this.code = code;
     }
+
 }
