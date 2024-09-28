ef8e9f082ed7c08bba9ed14ceec437cb86331934
Crestimes <1259864733@qq.com>
2024-09-28 22:08:58 +0800

    perf: 优化全局异常处理controller的异常处理逻辑

--- src/main/java/com/r/chat/controller/GlobalExceptionHandlerController.java
+++ src/main/java/com/r/chat/controller/GlobalExceptionHandlerController.java
@@ -1,15 +1,12 @@
 package com.r.chat.controller;
 
+import com.r.chat.entity.constants.Constants;
 import com.r.chat.entity.enums.ResponseCodeEnum;
 import com.r.chat.entity.vo.Result;
 import com.r.chat.exception.BusinessException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
-
-import java.net.BindException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandlerController {
@@ -17,16 +14,14 @@ public class GlobalExceptionHandlerController {
     public Result<String> handleException(Exception e) {
         if (e instanceof NoHandlerFoundException) {
             // 404
-            return Result.error(ResponseCodeEnum.NOT_FOUND.getCode(), ResponseCodeEnum.NOT_FOUND.getMessage());
-        } else if (e instanceof BindException || e instanceof MethodArgumentNotValidException || e instanceof HttpRequestMethodNotSupportedException) {
-            // 拒接请求
-            return Result.error(ResponseCodeEnum.NOT_ACCEPTABLE.getCode(), ResponseCodeEnum.NOT_ACCEPTABLE.getMessage());
-        } else if (e instanceof BusinessException) {
+            return Result.error(ResponseCodeEnum.NOT_FOUND.getCode(), Constants.MESSAGE_NOT_FOUND);
+        }else if (e instanceof BusinessException) {
             // 业务异常
             BusinessException be = (BusinessException) e;
-            return Result.error(ResponseCodeEnum.FORBIDDEN.getCode(), be.getMessage());
+            return Result.error(be.getCode(), be.getMessage());
         } else {
-            return Result.error(ResponseCodeEnum.INTERNAL_SERVER_ERROR.getCode(), ResponseCodeEnum.INTERNAL_SERVER_ERROR.getMessage());
+            // 内部错误
+            return Result.error(ResponseCodeEnum.INTERNAL_ERROR.getCode(), Constants.MESSAGE_INTERNAL_ERROR);
         }
     }
 }
