b2801c60ab84883cd58c90d4d0fb193bfec165c5
Crestimes <1259864733@qq.com>
2024-09-29 10:53:00 +0800

    feat: 注册自定义拦截器TokenInterceptor，修正静态资源映射方法

--- src/main/java/com/r/chat/config/WebMvcConfig.java
+++ src/main/java/com/r/chat/config/WebMvcConfig.java
@@ -1,19 +1,40 @@
 package com.r.chat.config;
 
+import com.r.chat.interceptor.TokenInterceptor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+@Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig extends WebMvcConfigurationSupport {
+    private final TokenInterceptor tokenInterceptor;
+
+    /**
+     * 注册自定义拦截器
+     *
+     * @param registry 注册表
+     */
+    protected void addInterceptors(InterceptorRegistry registry) {
+        log.info("注册自定义拦截器 TokenInterceptor");
+        registry.addInterceptor(tokenInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/swagger-resources/**","/swagger-ui/**", "/v3/**", "/error")  // NoHandlerFoundException映射到的error请求、knife4j的请求
+                .excludePathPatterns("/account/checkCode")  // 验证码
+                .excludePathPatterns("/account/register")  // 注册
+                .excludePathPatterns("/account/login");  // 登录
+    }
+
     /**
      * 设置静态资源映射
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations(
-                "classpath:/static/");
-        registry.addResourceHandler("swagger-ui.html", "doc.html").addResourceLocations(
+        registry.addResourceHandler("swagger-ui.html", "doc.html", "favicon.ico").addResourceLocations(
                 "classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations(
                 "classpath:/META-INF/resources/webjars/");
