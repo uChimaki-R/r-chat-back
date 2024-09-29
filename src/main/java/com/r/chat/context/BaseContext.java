9bc75eed9e0df788c636bae0eee6064fd35c3a01
Crestimes <1259864733@qq.com>
2024-09-29 10:50:56 +0800

    feat: 新增上下文类BaseContext记录当前请求的用户信息

---
+++ src/main/java/com/r/chat/context/BaseContext.java
@@ -0,0 +1,21 @@
+package com.r.chat.context;
+
+import com.r.chat.entity.vo.UserInfoToken;
+
+public class BaseContext {
+
+    public static ThreadLocal<UserInfoToken> threadLocal = new ThreadLocal<>();
+
+    public static void setCurrentUserInfoToken(UserInfoToken userInfoToken) {
+        threadLocal.set(userInfoToken);
+    }
+
+    public static UserInfoToken getCurrentUserInfoToken() {
+        return threadLocal.get();
+    }
+
+    public static void removeCurrentUserInfoToken() {
+        threadLocal.remove();
+    }
+
+}
