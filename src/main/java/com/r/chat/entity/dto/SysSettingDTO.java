32abe22ce2befdb5a5cacf89139104b6d76953f1
Crestimes <1259864733@qq.com>
2024-09-29 14:00:04 +0800

    feat: 新增系统设置DTO类

---
+++ src/main/java/com/r/chat/entity/dto/SysSettingDTO.java
@@ -0,0 +1,33 @@
+package com.r.chat.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SysSettingDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    // 个人最多群组数量
+    private Integer maxGroupCount;
+    // 单个群组最多群成员
+    private Integer maxGroupMemberCount;
+    // 图片最大大小
+    private Integer maxImageSize;
+    // 视频最大大小
+    private Integer maxVideoSize;
+    // 文件最大大小
+    private Integer maxFileSize;
+    // 初始机器人id
+    private String robotId;
+    // 初始机器人昵称
+    private String robotNickName;
+    // 初始机器人欢迎语
+    private String robotWelcomeMsg;
+}
