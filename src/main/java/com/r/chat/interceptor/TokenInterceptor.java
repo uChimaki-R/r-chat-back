b88ba7de70b2f843baba22c6c0f451c2eefd50fe
Crestimes <1259864733@qq.com>
2024-09-29 10:55:18 +0800

    perf: 重写afterCompletion方法，释放上下文对象

--- src/main/java/com/r/chat/interceptor/TokenInterceptor.java
+++ src/main/java/com/r/chat/interceptor/TokenInterceptor.java
@@ -26,7 +26,7 @@ public class TokenInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (!(handler instanceof HandlerMethod)) {
             // 没匹配到路径，给全局异常处理controller处理
-           return true;
+            return true;
         }
 
         // 从请求头中获取token
@@ -48,5 +48,11 @@ public class TokenInterceptor implements HandlerInterceptor {
             log.warn("无法获取该token对应的用户信息");
             throw new LoginTimeOutException(Constants.MESSAGE_NOT_LOGIN);
         }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // 释放上下文对象
+        BaseContext.removeCurrentUserInfoToken();
     }
 }
