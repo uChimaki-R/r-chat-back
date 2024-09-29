ce27f5647c1f4f2cb5d4856531d0316b0b7ee3b9
Crestimes <1259864733@qq.com>
2024-09-29 14:00:58 +0800

    feat: 新增getSysSetting方法用于获取系统设置信息，如果没有则从配置文件中获取默认系统设置信息，修改部分变量名称

--- src/main/java/com/r/chat/redis/RedisUtils.java
+++ src/main/java/com/r/chat/redis/RedisUtils.java
@@ -1,8 +1,11 @@
 package com.r.chat.redis;
 
 import com.r.chat.entity.constants.Constants;
+import com.r.chat.entity.dto.SysSettingDTO;
 import com.r.chat.entity.vo.UserInfoToken;
+import com.r.chat.properties.DefaultSysSettingProperties;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
@@ -14,12 +17,13 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class RedisUtils {
     private final RedisOperation redisOperation;
+    private final DefaultSysSettingProperties defaultSysSettingProperties;
 
     /**
      * 获取用户心跳
      */
     public Long getUserHeartBeat(String userId) {
-        return (Long) redisOperation.get(Constants.REDIS_KEY_WS_HEART_BEAT_PREFIX + userId);
+        return (Long) redisOperation.get(Constants.REDIS_KEY_PREFIX_WS_HEART_BEAT + userId);
     }
 
     /**
@@ -28,21 +32,34 @@ public class RedisUtils {
     public void saveUserTokenInfo(UserInfoToken userInfoToken) {
         // 保存两份信息，一份是用token找到用户信息，一份是userId找到token
         // 保存一天的登录信息
-        redisOperation.setEx(Constants.REDIS_KEY_USER_TOKEN_PREFIX + userInfoToken.getToken(), userInfoToken, 1, TimeUnit.DAYS);
-        redisOperation.setEx(Constants.REDIS_KEY_USER_ID_PREFIX + userInfoToken.getUserId(), userInfoToken.getToken(), 1, TimeUnit.DAYS);
+        redisOperation.setEx(Constants.REDIS_KEY_PREFIX_USER_TOKEN + userInfoToken.getToken(), userInfoToken, 1, TimeUnit.DAYS);
+        redisOperation.setEx(Constants.REDIS_KEY_PREFIX_USER_ID + userInfoToken.getUserId(), userInfoToken.getToken(), 1, TimeUnit.DAYS);
     }
 
     /**
      * 根据token获取用户信息
      */
     public UserInfoToken getUserInfoToken(String token) {
-        return (UserInfoToken) redisOperation.get(Constants.REDIS_KEY_USER_TOKEN_PREFIX + token);
+        return (UserInfoToken) redisOperation.get(Constants.REDIS_KEY_PREFIX_USER_TOKEN + token);
     }
 
     /**
      * 根据用户id获取token
      */
     public String getToken(String userId) {
-        return (String) redisOperation.get(Constants.REDIS_KEY_USER_ID_PREFIX + userId);
+        return (String) redisOperation.get(Constants.REDIS_KEY_PREFIX_USER_ID + userId);
+    }
+
+    /**
+     * 获取系统设置信息，如果没有则从配置文件中获取默认系统设置信息
+     */
+    public SysSettingDTO getSysSetting() {
+        SysSettingDTO sysSettingDTO = (SysSettingDTO) redisOperation.get(Constants.REDIS_KEY_SYS_SETTINGS);
+        if (sysSettingDTO == null) {
+            // 获取默认配置
+            sysSettingDTO = new SysSettingDTO();
+            BeanUtils.copyProperties(defaultSysSettingProperties, sysSettingDTO);
+        }
+        return sysSettingDTO;
     }
 }
