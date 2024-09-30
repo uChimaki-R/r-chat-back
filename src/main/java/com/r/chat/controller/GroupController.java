389da1b369f2eddd0c1838b96a7ab6f0402c881b
Crestimes <1259864733@qq.com>
2024-09-30 11:03:24 +0800

    feat: 新增loadMyGroup方法用于处理获取自己创建的群组的请求

--- src/main/java/com/r/chat/controller/GroupController.java
+++ src/main/java/com/r/chat/controller/GroupController.java
@@ -1,13 +1,18 @@
 package com.r.chat.controller;
 
+import com.r.chat.context.UserIdContext;
 import com.r.chat.entity.dto.GroupInfoDTO;
-import com.r.chat.entity.vo.Result;
+import com.r.chat.entity.po.GroupInfo;
+import com.r.chat.result.Result;
 import com.r.chat.service.IGroupInfoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @Slf4j
@@ -25,5 +30,15 @@ public class GroupController {
         log.info("群组操作: {}", groupInfoDTO);
         groupInfoService.saveOrUpdateGroupInfo(groupInfoDTO);
         return Result.success();
+    }
+
+    /**
+     * 获取自己创建的群组
+     */
+    @GetMapping("/loadMyGroup")
+    public Result<List<GroupInfo>> loadMyGroup() {
+        String ownerId = UserIdContext.getCurrentUserId();
+        List<GroupInfo> list = groupInfoService.lambdaQuery().eq(GroupInfo::getGroupOwnerId, ownerId).list();
+        return Result.success(list);
     }
 }
