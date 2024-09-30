d2185ae3fb9c753ee2a04c877257d2f38a05b8bd
Crestimes <1259864733@qq.com>
2024-09-30 23:42:33 +0800

    feat: 新增部分异常信息常量，修改部分常量值

--- src/main/java/com/r/chat/entity/constants/Constants.java
+++ src/main/java/com/r/chat/entity/constants/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     public static final String REDIS_KEY_SYS_SETTINGS = "rchat:sys:settings";  // 系统设置保存路径
 
     // length
-    public static final Integer LENGTH_ID = 11;  // 用户id及群组id的长度
+    public static final Integer LENGTH_ID = 11;  // 用户id及群聊id的长度
     public static final Integer LENGTH_TOKEN_RANDOM_CHARS = 20;  // token中拼接的随机字符串的长度
 
     // exception message
@@ -23,10 +23,13 @@ public class Constants {
     public static final String MESSAGE_ACCOUNT_ALREADY_LOGIN = "账号已在别处登录";
     public static final String MESSAGE_ACCOUNT_DISABLE = "账号被锁定";
     public static final String MESSAGE_NOT_LOGIN = "登录超时";
-    public static final String MESSAGE_GROUP_COUNT_LIMIT = "拥有的群组数量达到上限";
+    public static final String MESSAGE_GROUP_COUNT_LIMIT = "拥有的群聊数量达到上限";
     public static final String MESSAGE_MISSING_AVATAR_FILE = "缺少头像文件";
     public static final String MESSAGE_NOT_GROUP_OWNER_OPERATION = "非群主操作";
     public static final String MESSAGE_FAILED_TO_SAVE_AVATAR_FILE = "头像保存失败";
+    public static final String MESSAGE_GROUP_NOT_EXIST = "群聊不存在";
+    public static final String MESSAGE_GROUP_ALREADY_DISBAND = "群聊已解散";
+    public static final String MESSAGE_NOT_IN_THE_GROUP = "不在此群聊";
 
     // file
     public static final String FILE_FOLDER_AVATAR = "avatar";
