098805ede05637127d1f78afd52438d90e8874c3
Crestimes <1259864733@qq.com>
2024-09-30 11:18:58 +0800

    refactor: 重命名[UserInfoToken->UserTokenInfoDTO]，改变文件路径

--- src/main/java/com/r/chat/entity/dto/UserTokenInfoDTO.java
+++ src/main/java/com/r/chat/entity/dto/UserTokenInfoDTO.java
@@ -1,4 +1,4 @@
-package com.r.chat.entity.vo;
+package com.r.chat.entity.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,19 +9,21 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserInfoToken implements Serializable {
+public class UserTokenInfoDTO implements Serializable {
     private static final long serialVersionUID = 1L;
     private String userId;
     private String email;
-    private String password;
     private String nickName;
-    private Integer gender;
-    private String token;
     private Integer joinType; // 好友添加模式 0: 可以直接添加好友 1: 同意后添加好友
+    private Integer gender;
+    private String password;
     private String personalSignature;
     private String areaName;
     private String areaCode;
+    // 没有 status createTime lastLoginTime lastOffTime
 
+    // 多了以下属性
+    private String token;
     private boolean admin = false;
     private Integer contactStatus; // 当前状态
 }
