a201b2bb3e2afe94e8d5c55a0abd22fea001883b
Crestimes <1259864733@qq.com>
2024-10-01 18:15:34 +0800

    feat: 新增查询方法selectGroupMemberByGroupId用于根据群聊id获取群成员的id、昵称信息

--- src/main/java/com/r/chat/mapper/UserContactMapper.java
+++ src/main/java/com/r/chat/mapper/UserContactMapper.java
@@ -1,7 +1,10 @@
 package com.r.chat.mapper;
 
+import com.r.chat.entity.dto.GroupMemberInfoDTO;
 import com.r.chat.entity.po.UserContact;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface UserContactMapper extends BaseMapper<UserContact> {
 
+    /**
+     * 根据群聊id获取群成员的id、昵称信息
+     */
+    List<GroupMemberInfoDTO> selectGroupMemberByGroupId(String groupId);
 }
