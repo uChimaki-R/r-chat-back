5381c4b467f259a6d2747572cefc4444abf846bf
Crestimes <1259864733@qq.com>
2024-10-01 22:17:31 +0800

    feat: 新增联系人controller，添加search方法用于处理搜索用户或群组的请求

---
+++ src/main/java/com/r/chat/controller/ContactController.java
@@ -0,0 +1,31 @@
+package com.r.chat.controller;
+
+import com.r.chat.entity.dto.ContactSearchResultDTO;
+import com.r.chat.entity.result.Result;
+import com.r.chat.entity.vo.ContactSearchResultVO;
+import com.r.chat.service.IUserContactService;
+import com.r.chat.utils.CopyUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/contact")
+@RequiredArgsConstructor
+public class ContactController {
+    private final IUserContactService userContactService;
+
+    /**
+     * 搜索用户或群组
+     */
+    @GetMapping("/search")
+    public Result<ContactSearchResultVO> search(String contactId) {
+        ContactSearchResultDTO contactSearchResultDTO = userContactService.search(contactId);
+        ContactSearchResultVO contactSearchResultVO = CopyUtils.copyBean(contactSearchResultDTO, ContactSearchResultVO.class);
+        return Result.success(contactSearchResultVO);
+    }
+
+}
