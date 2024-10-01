41657e9fa357f606244aa73764b795c8236f853d
Crestimes <1259864733@qq.com>
2024-10-01 10:31:52 +0800

    docs: 添加和修改部分注释

--- src/main/java/com/r/chat/entity/dto/UserTokenInfoDTO.java
+++ src/main/java/com/r/chat/entity/dto/UserTokenInfoDTO.java
@@ -11,19 +11,63 @@ import java.io.Serializable;
 @NoArgsConstructor
 public class UserTokenInfoDTO implements Serializable {
     private static final long serialVersionUID = 1L;
+    /**
+     * 用户id
+     */
     private String userId;
-    private String email;
-    private String nickName;
-    private Integer joinType; // 好友添加模式 0: 可以直接添加好友 1: 同意后添加好友
-    private Integer gender;
-    private String password;
-    private String personalSignature;
-    private String areaName;
-    private String areaCode;
-    // 没有 status createTime lastLoginTime lastOffTime
 
-    // 多了以下属性
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 用户昵称
+     */
+    private String nickName;
+
+    /**
+     * 好友添加模式 0: 可以直接添加好友 1: 同意后添加好友
+     */
+    private Integer joinType;
+
+    /**
+     * 性别
+     */
+    private Integer gender;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 个性签名
+     */
+    private String personalSignature;
+
+    /**
+     * 地区名
+     */
+    private String areaName;
+
+    /**
+     * 地区编号
+     */
+    private String areaCode;
+
+    /**
+     * token
+     */
     private String token;
+
+    /**
+     * 是否是管理员账号
+     */
     private boolean admin = false;
-    private Integer contactStatus; // 当前状态
+
+    /**
+     * 当前状态
+     */
+    private Integer contactStatus;
 }
