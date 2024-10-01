14545a51ddef7de57d86d49ee424c463684ed23b
Crestimes <1259864733@qq.com>
2024-10-01 22:15:57 +0800

    fix: 指定UserContactTypeEnum枚举类的json化展示字段为value

--- src/main/java/com/r/chat/entity/enums/UserContactTypeEnum.java
+++ src/main/java/com/r/chat/entity/enums/UserContactTypeEnum.java
@@ -1,6 +1,7 @@
 package com.r.chat.entity.enums;
 
 import com.baomidou.mybatisplus.annotation.EnumValue;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 
 @Getter
@@ -8,6 +9,7 @@ public enum UserContactTypeEnum {
     FRIENDS(0, "好友"),
     GROUP(1, "群聊");
     @EnumValue
+    @JsonValue
     private final int value;
     private final String desc;
 
