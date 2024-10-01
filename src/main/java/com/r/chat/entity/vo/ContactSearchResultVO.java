5f616fee7a9d84319c6834cf83755e5dc1a653e7
Crestimes <1259864733@qq.com>
2024-10-01 22:16:46 +0800

    feat: 新增联系人搜索结果VO类

---
+++ src/main/java/com/r/chat/entity/vo/ContactSearchResultVO.java
@@ -0,0 +1,46 @@
+package com.r.chat.entity.vo;
+
+import com.r.chat.entity.enums.UserContactStatusEnum;
+import com.r.chat.entity.enums.UserContactTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContactSearchResultVO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 联系人id
+     */
+    private String contactId;
+
+    /**
+     * 该联系人的类别 用户/群聊
+     */
+    private UserContactTypeEnum contactType;
+
+    /**
+     * 用户名/群聊名
+     */
+    private String nickName;
+
+    /**
+     * 和该联系人的关系状态
+     */
+    private UserContactStatusEnum status;
+
+    /**
+     * 联系人性别
+     */
+    private Integer gender;
+
+    /**
+     * 联系人地区
+     */
+    private String areaName;
+}
