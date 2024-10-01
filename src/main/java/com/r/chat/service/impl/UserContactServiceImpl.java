f7903a5c3b47007d590b4784a0e5bce3d29b83ac
Crestimes <1259864733@qq.com>
2024-10-01 18:15:08 +0800

    feat: 实现接口方法getGroupMemberInfo

--- src/main/java/com/r/chat/service/impl/UserContactServiceImpl.java
+++ src/main/java/com/r/chat/service/impl/UserContactServiceImpl.java
@@ -1,10 +1,14 @@
 package com.r.chat.service.impl;
 
+import com.r.chat.entity.dto.GroupMemberInfoDTO;
 import com.r.chat.entity.po.UserContact;
 import com.r.chat.mapper.UserContactMapper;
 import com.r.chat.service.IUserContactService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -15,6 +19,12 @@ import org.springframework.stereotype.Service;
  * @since 2024-09-28
  */
 @Service
+@RequiredArgsConstructor
 public class UserContactServiceImpl extends ServiceImpl<UserContactMapper, UserContact> implements IUserContactService {
+    private final UserContactMapper userContactMapper;
 
+    @Override
+    public List<GroupMemberInfoDTO> getGroupMemberInfo(String groupId) {
+        return userContactMapper.selectGroupMemberByGroupId(groupId);
+    }
 }
