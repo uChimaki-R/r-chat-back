dc6046f958eb930f00c0672f44a388e9c3ec4473
Crestimes <1259864733@qq.com>
2024-10-01 22:03:04 +0800

    fix: 修改id前缀枚举类的属性prefix为Character类，修正相关的方法

--- src/main/java/com/r/chat/entity/enums/IdPrefixEnum.java
+++ src/main/java/com/r/chat/entity/enums/IdPrefixEnum.java
@@ -4,13 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum IdPrefixEnum {
-    USER(0, "U", "用户"),
-    GROUP(1, "G", "群聊");
+    USER(0, 'U', "用户"),
+    GROUP(1, 'G', "群聊");
     private final int id;
-    private final String prefix;
+    private final Character prefix;
     private final String desc;
 
-    IdPrefixEnum(int id, String prefix, String desc) {
+    IdPrefixEnum(int id, Character prefix, String desc) {
         this.id = id;
         this.prefix = prefix;
         this.desc = desc;
@@ -25,7 +25,7 @@ public enum IdPrefixEnum {
         return null;
     }
 
-    public static IdPrefixEnum getByPrefix(String prefix) {
+    public static IdPrefixEnum getByPrefix(Character prefix) {
         for (IdPrefixEnum e : IdPrefixEnum.values()) {
             if (e.prefix.equals(prefix)) {
                 return e;
