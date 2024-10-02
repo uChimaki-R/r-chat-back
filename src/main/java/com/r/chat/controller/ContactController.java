746cda016b80ab7b19453dbf6468b3a0abefe6ba
Crestimes <1259864733@qq.com>
2024-10-02 16:49:31 +0800

    feat: 新增applyAdd用于处理请求添加用户或群组的请求。为部分变量添加NotEmpty注解。

--- src/main/java/com/r/chat/controller/ContactController.java
+++ src/main/java/com/r/chat/controller/ContactController.java
@@ -1,6 +1,8 @@
 package com.r.chat.controller;
 
+import com.r.chat.entity.dto.ApplyDTO;
 import com.r.chat.entity.dto.ContactSearchResultDTO;
+import com.r.chat.entity.enums.JoinTypeEnum;
 import com.r.chat.entity.result.Result;
 import com.r.chat.entity.vo.ContactSearchResultVO;
 import com.r.chat.service.IUserContactService;
@@ -8,8 +10,11 @@ import com.r.chat.utils.CopyUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotEmpty;
 
 @Slf4j
 @RestController
@@ -22,11 +27,22 @@ public class ContactController {
      * 搜索用户或群组
      */
     @GetMapping("/search")
-    public Result<ContactSearchResultVO> search(String contactId) {
+    public Result<ContactSearchResultVO> search(@NotEmpty String contactId) {
         log.info("搜索用户或群组 contactId: {}", contactId);
         ContactSearchResultDTO contactSearchResultDTO = userContactService.search(contactId);
         ContactSearchResultVO contactSearchResultVO = CopyUtils.copyBean(contactSearchResultDTO, ContactSearchResultVO.class);
         return Result.success(contactSearchResultVO);
+    }
+
+    /**
+     * 请求添加用户或群组
+     *
+     * @return 返回添加类型的枚举，0为可以直接添加，1需要对方确认后才能添加
+     */
+    @PostMapping("/applyAdd")
+    public Result<JoinTypeEnum> applyAdd(ApplyDTO applyDTO) {
+        log.info("添加用户或群组 applyDTO: {}", applyDTO);
+        return Result.success(userContactService.applyAdd(applyDTO));
     }
 
 }
