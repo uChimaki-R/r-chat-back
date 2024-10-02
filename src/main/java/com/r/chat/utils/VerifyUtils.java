a7b868c5dbb654ff7b0d7da39dd16454f18ca15d
Crestimes <1259864733@qq.com>
2024-10-02 13:48:55 +0800

    perf: 重命名[isCurrentUser->assertIsCurrentUser]，优化代码逻辑，在断言不成功时抛出错误

--- src/main/java/com/r/chat/utils/VerifyUtils.java
+++ src/main/java/com/r/chat/utils/VerifyUtils.java
@@ -1,12 +1,20 @@
 package com.r.chat.utils;
 
 import com.r.chat.context.UserIdContext;
+import com.r.chat.entity.constants.Constants;
+import com.r.chat.exception.IllegalOperationException;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class VerifyUtils {
     /**
-     * 判断传入的用户id是否和当前的请求用户id相同，排除使用api操作他人数据的情况
+     * 断言传入的用户id和当前的请求用户id相同，不同时会抛出异常
+     * 以此排除使用api操作他人数据的情况
      */
-    public static boolean isCurrentUser(String userId) {
-        return UserIdContext.getCurrentUserId().equals(userId);
+    public static void assertIsCurrentUser(String userId) {
+        if (!UserIdContext.getCurrentUserId().equals(userId)) {
+            log.warn("非法操作 合法操作者userId: {}", userId);
+            throw new IllegalOperationException(Constants.MESSAGE_ILLEGAL_OPERATION);
+        }
     }
 }
