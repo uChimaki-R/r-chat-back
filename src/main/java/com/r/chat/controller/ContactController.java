90c7fde48942bc6828bd75043c9ec9804a0f36ba
Crestimes <1259864733@qq.com>
2024-10-07 13:37:27 +0800

    feat: 新增loadApply方法用于处理加载申请信息的请求。 refactor: 重命名部分变量。

--- src/main/java/com/r/chat/controller/ContactController.java
+++ src/main/java/com/r/chat/controller/ContactController.java
@@ -1,13 +1,18 @@
 package com.r.chat.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.r.chat.entity.constants.Constants;
 import com.r.chat.entity.dto.ApplyDTO;
 import com.r.chat.entity.dto.ContactSearchResultDTO;
 import com.r.chat.entity.dto.BasicInfoDTO;
 import com.r.chat.entity.enums.JoinTypeEnum;
 import com.r.chat.entity.enums.UserContactTypeEnum;
+import com.r.chat.entity.result.PageResult;
 import com.r.chat.entity.result.Result;
+import com.r.chat.entity.vo.ContactApplyVO;
 import com.r.chat.entity.vo.ContactSearchResultVO;
 import com.r.chat.entity.vo.BasicInfoVO;
+import com.r.chat.service.IUserContactApplyService;
 import com.r.chat.service.IUserContactService;
 import com.r.chat.utils.CopyUtils;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +30,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ContactController {
     private final IUserContactService userContactService;
+    private final IUserContactApplyService userContactApplyService;
 
     /**
      * 搜索用户或群聊
@@ -49,12 +55,36 @@ public class ContactController {
     }
 
     /**
+     * 加载申请信息
+     */
+    @GetMapping("/loadApply")
+    public Result<PageResult<ContactApplyVO>> loadApply(Long pageNo, Long pageSize) {
+        // 加人的申请返回申请人的信息，加群申请不仅返回申请人的信息，还要返回是想加入哪个群的信息
+        // 这里和下面的loadContact不同，loadContact是加载自己的好友/群聊信息，这里是加载别人申请的信息，如果自己是群主的话还会接收到加群的申请
+        // 作为群主的自己，想看到的肯定是"谁"想加入我的"哪一个群聊"两个信息，所以统一一起返回
+        pageNo = pageNo == null ? 1 : pageNo;
+        pageSize = pageSize == null ? Constants.SIZE_DEFAULT_PAGE_SIZE : pageSize;
+        log.info("加载申请信息 pageNo: {}, pageSize: {}", pageNo, pageSize);
+        // 涉及用户名称/群聊名称的填充，需要多表联查
+        Page<ContactApplyVO> page = userContactApplyService.getApplyInfoPage(new Page<>(pageNo, pageSize));
+        // 包装成自己的pageResult对象
+        PageResult<ContactApplyVO> pageResult = new PageResult<>();
+        pageResult.setPageSize(pageSize);
+        pageResult.setPageNo(pageNo);
+        pageResult.setPageTotal(page.getPages());
+        pageResult.setTotalCount(page.getTotal());
+        pageResult.setData(page.getRecords());
+        log.info("获取到申请信息 pageResult: {}", pageResult);
+        return Result.success(pageResult);
+    }
+
+    /**
      * 加载好友或加入的群聊
      */
     @GetMapping("/loadContact")
-    public Result<List<BasicInfoVO>> loadContact(UserContactTypeEnum userContactType) {
+    public Result<List<BasicInfoVO>> loadContact(UserContactTypeEnum contactType) {
         log.info("查询好友/加入的群聊");
-        List<BasicInfoDTO> basicInfoDTOList = userContactService.loadContact(userContactType);
+        List<BasicInfoDTO> basicInfoDTOList = userContactService.loadContact(contactType);
         List<BasicInfoVO> basicInfoVOList = CopyUtils.copyList(basicInfoDTOList, BasicInfoVO.class);
         return Result.success(basicInfoVOList);
     }
