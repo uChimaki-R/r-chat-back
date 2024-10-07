e9a74eae4a895335c80c3a7aca87c71e4b4b6924
Crestimes <1259864733@qq.com>
2024-10-07 13:39:04 +0800

    feat: 实现getApplyInfoPage接口方法

--- src/main/java/com/r/chat/service/impl/UserContactApplyServiceImpl.java
+++ src/main/java/com/r/chat/service/impl/UserContactApplyServiceImpl.java
@@ -1,9 +1,13 @@
 package com.r.chat.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.r.chat.context.UserIdContext;
 import com.r.chat.entity.po.UserContactApply;
+import com.r.chat.entity.vo.ContactApplyVO;
 import com.r.chat.mapper.UserContactApplyMapper;
 import com.r.chat.service.IUserContactApplyService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +19,12 @@ import org.springframework.stereotype.Service;
  * @since 2024-09-28
  */
 @Service
+@RequiredArgsConstructor
 public class UserContactApplyServiceImpl extends ServiceImpl<UserContactApplyMapper, UserContactApply> implements IUserContactApplyService {
+    private final UserContactApplyMapper userContactApplyMapper;
 
+    @Override
+    public Page<ContactApplyVO> getApplyInfoPage(Page<ContactApplyVO> page) {
+        return userContactApplyMapper.selectApplyInfoPage(page, UserIdContext.getCurrentUserId());
+    }
 }
