9998e5f7a999f0f98b535a3c7131a628f83a14e0
CodexploRe <1259864733@qq.com>
2024-10-08 16:10:05 +0800

    feat: 新增异常信息常量MESSAGE_GROUP_MEMBER_COUNT_LIMIT

--- src/main/java/com/r/chat/entity/constants/Constants.java
+++ src/main/java/com/r/chat/entity/constants/Constants.java
@@ -29,6 +29,7 @@ public class Constants {
     public static final String MESSAGE_ACCOUNT_DISABLE = "账号被锁定";
     public static final String MESSAGE_NOT_LOGIN = "登录超时";
     public static final String MESSAGE_GROUP_COUNT_LIMIT = "拥有的群聊数量达到上限: %d";
+    public static final String MESSAGE_GROUP_MEMBER_COUNT_LIMIT = "群聊成员数量达到上限: %d";
     public static final String MESSAGE_MISSING_AVATAR_FILE = "缺少头像文件";
     public static final String MESSAGE_FAILED_TO_SAVE_AVATAR_FILE = "头像保存失败";
     public static final String MESSAGE_GROUP_NOT_EXIST = "群聊不存在";
