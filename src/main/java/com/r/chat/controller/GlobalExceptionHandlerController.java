374ae4dc80ea838661b42cfbe8241152e7a5163b
Crestimes <1259864733@qq.com>
2024-10-10 16:32:09 +0800

    feat: 新增验证失败时抛出的错误的分支，返回对应的错误信息。新增部分日志输出。

--- src/main/java/com/r/chat/controller/GlobalExceptionHandlerController.java
+++ src/main/java/com/r/chat/controller/GlobalExceptionHandlerController.java
@@ -4,24 +4,42 @@ import com.r.chat.entity.constants.Constants;
 import com.r.chat.entity.enums.ResponseCodeEnum;
 import com.r.chat.entity.result.Result;
 import com.r.chat.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import javax.validation.ConstraintViolationException;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandlerController {
     @ExceptionHandler(Exception.class)
     public Result<String> handleException(Exception e) {
         if (e instanceof NoHandlerFoundException) {
             // 404
-            return Result.error(ResponseCodeEnum.NOT_FOUND.getCode(), Constants.MESSAGE_NOT_FOUND);
+            Result<String> result = Result.error(ResponseCodeEnum.NOT_FOUND.getCode(), Constants.MESSAGE_NOT_FOUND);
+            log.error("NoHandlerFoundException | {}", result);
+            return result;
         }else if (e instanceof BusinessException) {
             // 业务异常
             BusinessException be = (BusinessException) e;
-            return Result.error(be.getCode(), be.getMessage());
-        } else {
+            Result<String> result = Result.error(be.getCode(), be.getMessage());
+            log.error("BusinessException | {}", result);
+            return result;
+        } else if (e instanceof ConstraintViolationException) {
+            // 参数错误
+            ConstraintViolationException cve = (ConstraintViolationException) e;
+            String[] ms = cve.getMessage().split(" ");
+            Result<String> result = Result.error(ResponseCodeEnum.PARAMETERS_ERROR.getCode(), ms[ms.length - 1]);
+            log.error("ConstraintViolationException | {}", result);
+            return result;
+        }
+        else {
             // 内部错误
-            return Result.error(ResponseCodeEnum.INTERNAL_ERROR.getCode(), Constants.MESSAGE_INTERNAL_ERROR);
+            Result<String> result = Result.error(ResponseCodeEnum.INTERNAL_ERROR.getCode(), Constants.MESSAGE_INTERNAL_ERROR);
+            log.error("Error | {}", result);
+            return result;
         }
     }
 }
