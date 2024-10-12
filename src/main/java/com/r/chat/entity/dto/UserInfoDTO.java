efebbbb5bde7ce74df35246f8c8a4c669c51e075
Crestimes <1259864733@qq.com>
2024-10-12 09:46:07 +0800

    feat: 新增用户信息DTO类，用于接收前端修改的用户信息，包括头像文件等。设置性别数据的校验逻辑：要求为数字0或1。

---
+++ src/main/java/com/r/chat/entity/dto/UserInfoDTO.java
@@ -0,0 +1,55 @@
+package com.r.chat.entity.dto;
+
+import com.r.chat.entity.constants.Constants;
+import com.r.chat.entity.enums.JoinTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfoDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 用户名
+     */
+    private String nickName;
+
+    /**
+     * 好友添加模式 0: 可以直接添加好友 1: 同意后添加好友
+     */
+    private JoinTypeEnum joinType;
+
+    /**
+     * 性别 0：女 1：男
+     */
+    @Range(min = 0, max = 1, message = Constants.VALIDATE_ILLEGAL_GENDER)
+    private Integer gender;
+
+    /**
+     * 个性签名
+     */
+    private String personalSignature;
+
+    /**
+     * 地区名
+     */
+    private String areaName;
+
+    /**
+     * 地区编号
+     */
+    private String areaCode;
+
+    /**
+     * 头像，一个原始，一个压缩过
+     */
+    private MultipartFile avatarFile;
+    private MultipartFile avatarCover;
+}
