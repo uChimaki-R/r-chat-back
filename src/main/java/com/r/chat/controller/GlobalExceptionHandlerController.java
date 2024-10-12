b37b9edc006b7a2510c93ec70fd6ecad102b27d2
Crestimes <1259864733@qq.com>
2024-10-12 09:47:56 +0800

    fix: 修正校验异常的分支判断条件，只要是来自spring或javax的validation包的异常都进入该分支，修正其中的代码逻辑。 refactor: 修改部分日志输出内容和日志输出等级。

--- src/main/java/com/r/chat/controller/GlobalExceptionHandlerController.java
+++ src/main/java/com/r/chat/controller/GlobalExceptionHandlerController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-import javax.validation.ConstraintViolationException;
 
 @Slf4j
 @RestControllerAdvice
@@ -19,26 +18,28 @@ public class GlobalExceptionHandlerController {
         if (e instanceof NoHandlerFoundException) {
             // 404
             Result<String> result = Result.error(ResponseCodeEnum.NOT_FOUND.getCode(), Constants.MESSAGE_NOT_FOUND);
-            log.error("NoHandlerFoundException | {}", result);
+            log.warn("NoHandlerFoundException | {}", result);
             return result;
-        }else if (e instanceof BusinessException) {
+        } else if (e instanceof BusinessException) {
             // 业务异常
             BusinessException be = (BusinessException) e;
             Result<String> result = Result.error(be.getCode(), be.getMessage());
-            log.error("BusinessException | {}", result);
+            log.warn("BusinessException | {}", result);
             return result;
-        } else if (e instanceof ConstraintViolationException) {
-            // 参数错误
-            ConstraintViolationException cve = (ConstraintViolationException) e;
-            String[] ms = cve.getMessage().split(" ");
-            Result<String> result = Result.error(ResponseCodeEnum.PARAMETERS_ERROR.getCode(), ms[ms.length - 1]);
-            log.error("ConstraintViolationException | {}", result);
+        } else if (e.getClass().getPackage().getName().startsWith("org.springframework.validation") ||
+                e.getClass().getPackage().getName().startsWith("javax.validation")) {
+            // 验证数据格式错误
+            String[] ms = e.getMessage().split(" ");
+            String message = ms[ms.length - 1];
+            if (message.startsWith("[")) message = message.substring(1, message.length() - 1);
+            if (message.endsWith("]")) message = message.substring(0, message.length() - 1);
+            Result<String> result = Result.error(ResponseCodeEnum.PARAMETERS_ERROR.getCode(), message);
+            log.warn("ValidationException | {}", result);
             return result;
-        }
-        else {
+        } else {
             // 内部错误
             Result<String> result = Result.error(ResponseCodeEnum.INTERNAL_ERROR.getCode(), Constants.MESSAGE_INTERNAL_ERROR);
-            log.error("Error | {}", result);
+            log.error("UnexpectedException | {}", result);
             return result;
         }
     }
