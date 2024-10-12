2a51ed6b09327e0e4442d0b21d9c008fa4862508
Crestimes <1259864733@qq.com>
2024-10-12 09:45:17 +0800

    feat: 补充@TableField注解。设置个性签名属性可以用空字符串更新，为null时不更新

--- src/main/java/com/r/chat/entity/po/UserInfo.java
+++ src/main/java/com/r/chat/entity/po/UserInfo.java
@@ -1,9 +1,8 @@
 package com.r.chat.entity.po;
 
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.time.LocalDateTime;
-import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -39,41 +38,49 @@ public class UserInfo implements Serializable {
     /**
      * 邮箱
      */
+    @TableField("email")
     private String email;
 
     /**
      * 用户名
      */
+    @TableField("nick_name")
     private String nickName;
 
     /**
      * 好友添加模式 0: 可以直接添加好友 1: 同意后添加好友
      */
+    @TableField("join_type")
     private JoinTypeEnum joinType;
 
     /**
      * 性别 0：女 1：男
      */
+    @TableField("gender")
     private Integer gender;
 
     /**
      * 密码
      */
+    @TableField("password")
     private String password;
 
     /**
      * 个性签名
      */
+    @TableField(value = "personal_signature", updateStrategy = FieldStrategy.NOT_NULL)  // 个性签名可以为空字符串，为null不更新（已经全局设置null/空字符串不更新）
     private String personalSignature;
 
     /**
      * 账号状态
      */
+    @TableField("status")
     private UserInfoStatusEnum status; // 使用枚举类型接收
 
     /**
      * 创建时间
      */
+    @TableField("create_time")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
@@ -81,6 +88,7 @@ public class UserInfo implements Serializable {
     /**
      * 最后登录时间
      */
+    @TableField("last_login_time")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime lastLoginTime;
@@ -88,16 +96,19 @@ public class UserInfo implements Serializable {
     /**
      * 最后下线时间（使用bigint记录到毫秒时间）
      */
+    @TableField("last_off_time")
     private Long lastOffTime;
 
     /**
      * 地区名
      */
+    @TableField("area_name")
     private String areaName;
 
     /**
      * 地区编号
      */
+    @TableField("area_code")
     private String areaCode;
 
 
