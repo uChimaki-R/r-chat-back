1ad6cf59a0218f712bdddb99fd9fb968fe167e9e
Crestimes <1259864733@qq.com>
2024-10-14 15:17:08 +0800

    feat: 新增修改密码信息DTO类

---
+++ src/main/java/com/r/chat/entity/dto/PasswordUpdateDTO.java
@@ -0,0 +1,24 @@
+package com.r.chat.entity.dto;
+
+import com.r.chat.entity.constants.Constants;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasswordUpdateDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @NotEmpty
+    private String oldPassword;
+
+    @NotEmpty(message = Constants.VALIDATE_EMPTY_PASSWORD)
+    @Pattern(regexp = Constants.REGEX_PASSWORD, message = Constants.VALIDATE_ILLEGAL_PASSWORD)
+    private String newPassword;
+}
