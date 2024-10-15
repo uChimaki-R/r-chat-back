ad8b41c135df596658d3607b1a27211d3c7e36dc
CodexploRe <1259864733@qq.com>
2024-10-15 17:17:31 +0800

    refactor: 修改loadContact方法参数类型为UserContactTypeEnum。

--- src/main/java/com/r/chat/service/IUserContactService.java
+++ src/main/java/com/r/chat/service/IUserContactService.java
@@ -3,6 +3,7 @@ package com.r.chat.service;
 import com.r.chat.entity.dto.*;
 import com.r.chat.entity.enums.JoinTypeEnum;
 import com.r.chat.entity.enums.UserContactStatusEnum;
+import com.r.chat.entity.enums.UserContactTypeEnum;
 import com.r.chat.entity.po.UserContact;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -36,7 +37,7 @@ public interface IUserContactService extends IService<UserContact> {
     /**
      * 获取好友或加入的群聊的信息
      */
-    List<BasicInfoDTO> loadContact(ContactTypeDTO contactTypeDTO);
+    List<BasicInfoDTO> loadContact(UserContactTypeEnum contactType);
 
     /**
      * 添加联系人
