8527e5274b161bb63fa5d451847395a6cbd3f59a
Crestimes <1259864733@qq.com>
2024-10-15 20:57:36 +0800

    feat: 新增loadBeautyAccountList方法用于处理加载靓号信息的请求。

--- src/main/java/com/r/chat/controller/AdminController.java
+++ src/main/java/com/r/chat/controller/AdminController.java
@@ -4,8 +4,10 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.r.chat.entity.constants.Constants;
 import com.r.chat.entity.dto.UserStatusDTO;
 import com.r.chat.entity.po.UserInfo;
+import com.r.chat.entity.po.UserInfoBeauty;
 import com.r.chat.entity.result.PageResult;
 import com.r.chat.entity.result.Result;
+import com.r.chat.service.IUserInfoBeautyService;
 import com.r.chat.service.IUserInfoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +24,7 @@ import javax.validation.constraints.NotEmpty;
 @RequiredArgsConstructor
 public class AdminController {
     private final IUserInfoService userInfoService;
+    private final IUserInfoBeautyService userInfoBeautyService;
 
     /**
      * 加载用户的信息
@@ -35,6 +38,21 @@ public class AdminController {
                 .page(new Page<>(pageNo, pageSize));
         PageResult<UserInfo> pageResult = PageResult.fromPage(page);
         log.info("获取到用户信息 {}", pageResult);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 加载靓号信息
+     */
+    @GetMapping("/loadBeautyAccountList")
+    public Result<PageResult<UserInfoBeauty>> loadBeautyAccountList(@RequestParam(defaultValue = "1") Long pageNo,
+                                                                    @RequestParam(defaultValue = "15") Long pageSize) {
+        log.info("获取靓号信息 pageNo: {}, pageSize: {}", pageNo, pageSize);
+        Page<UserInfoBeauty> page = userInfoBeautyService.lambdaQuery()
+                .orderBy(true, true, UserInfoBeauty::getId)
+                .page(new Page<>(pageNo, pageSize));
+        PageResult<UserInfoBeauty> pageResult = PageResult.fromPage(page);
+        log.info("获取到靓号信息 {}", pageResult);
         return Result.success(pageResult);
     }
 
