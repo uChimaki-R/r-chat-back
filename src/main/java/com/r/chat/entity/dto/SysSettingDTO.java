8af27d553240076e5452402b97917cf3e4bd830c
Crestimes <1259864733@qq.com>
2024-10-20 14:25:00 +0800

    feat: 新增avatarFile、avatarCover属性，用于接收机器人图片信息。实现AvatarOwner接口，实现getIdentityName方法，返回机器人的id。 docs: 添加和修改部分注释。

--- src/main/java/com/r/chat/entity/dto/SysSettingDTO.java
+++ src/main/java/com/r/chat/entity/dto/SysSettingDTO.java
@@ -1,32 +1,67 @@
 package com.r.chat.entity.dto;
 
+import com.r.chat.utils.AvatarOwner;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class SysSettingDTO implements Serializable {
+public class SysSettingDTO implements Serializable, AvatarOwner {
     private static final long serialVersionUID = 1L;
 
-    // 个人最多群聊数量
+    /**
+     * 个人最多群聊数量
+     */
     private Integer maxGroupCount;
-    // 单个群聊最多群成员
+
+    /**
+     * 单个群聊最多群成员
+     */
     private Integer maxGroupMemberCount;
-    // 图片最大大小
+
+    /**
+     * 图片最大大小
+     */
     private Integer maxImageSize;
-    // 视频最大大小
+
+    /**
+     * 视频最大大小
+     */
     private Integer maxVideoSize;
-    // 文件最大大小
+
+    /**
+     * 文件最大大小
+     */
     private Integer maxFileSize;
-    // 初始机器人id
+
+    /**
+     * 初始机器人id
+     */
     private String robotId;
-    // 初始机器人昵称
+
+    /**
+     * 初始机器人昵称
+     */
     private String robotNickName;
-    // 初始机器人欢迎语
+
+    /**
+     * 初始机器人欢迎语
+     */
     private String robotWelcomeMsg;
+
+    /**
+     * 机器人头像，一个压缩过一个没有
+     */
+    private MultipartFile avatarFile;
+    private MultipartFile avatarCover;
+
+    @Override
+    public String getIdentityName() {
+        return robotId;
+    }
 }
