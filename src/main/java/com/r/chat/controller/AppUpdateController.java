c4cc224351f77cb14c07dfb90dd249682dd2972b
Crestimes <1259864733@qq.com>
2024-10-20 16:27:35 +0800

    feat: 新增loadUpdateList方法用于加载app更新信息列表。 refactor: 修改controller接收请求的根路径[/app->/admin/app]

--- src/main/java/com/r/chat/controller/AppUpdateController.java
+++ src/main/java/com/r/chat/controller/AppUpdateController.java
@@ -1,8 +1,18 @@
 package com.r.chat.controller;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.r.chat.entity.dto.AppUpdateQueryDTO;
+import com.r.chat.entity.po.AppUpdate;
+import com.r.chat.entity.result.PageResult;
+import com.r.chat.entity.result.Result;
+import com.r.chat.service.IAppUpdateService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,7 +24,27 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-10-20
  */
 @RestController
-@RequestMapping("/app")
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/admin/app")
 public class AppUpdateController {
+    private final IAppUpdateService appUpdateService;
 
+    /**
+     * 加载app更新信息列表
+     */
+    @GetMapping("/loadUpdateList")
+    public Result<PageResult<AppUpdate>> loadUpdateList(AppUpdateQueryDTO appUpdateQueryDTO,
+                                                        @RequestParam(defaultValue = "1") Long pageNo,
+                                                        @RequestParam(defaultValue = "15") Long pageSize) {
+        log.info("获取app版本信息 pageNo: {}, pageSize: {}, {}", pageNo, pageSize, appUpdateQueryDTO);
+        Page<AppUpdate> page = appUpdateService.lambdaQuery()
+                .ge(appUpdateQueryDTO.getStartTime() != null, AppUpdate::getCreateTime, appUpdateQueryDTO.getStartTime())
+                .le(appUpdateQueryDTO.getEndTime() != null, AppUpdate::getCreateTime, appUpdateQueryDTO.getEndTime())
+                .orderByDesc(AppUpdate::getCreateTime)
+                .page(new Page<>(pageNo, pageSize));
+        PageResult<AppUpdate> pageResult = PageResult.fromPage(page);
+        log.info("获取到app版本信息 {}", pageResult);
+        return Result.success(pageResult);
+    }
 }
