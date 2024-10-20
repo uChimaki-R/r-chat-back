1d33af68d017e06331555e0f65f3d3c588f76e31
Crestimes <1259864733@qq.com>
2024-10-20 16:21:42 +0800

    fix: 补充LocalDateTime格式化注解

--- src/main/java/com/r/chat/entity/vo/UserInfoVO.java
+++ src/main/java/com/r/chat/entity/vo/UserInfoVO.java
@@ -1,10 +1,12 @@
 package com.r.chat.entity.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.r.chat.entity.enums.JoinTypeEnum;
 import com.r.chat.entity.enums.UserStatusEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -59,11 +61,15 @@ public class UserInfoVO implements Serializable {
     /**
      * 创建时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
 
     /**
      * 最后登录时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime lastLoginTime;
 
     /**
