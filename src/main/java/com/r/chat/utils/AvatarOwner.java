2426d5fd8a2da3a3c3d5710124109914cedefe1f
Crestimes <1259864733@qq.com>
2024-10-20 14:22:28 +0800

    feat: 新增AvatarOwner接口，需要实现获取图片文件、获取压缩过的图片文件、获取标识图片文件的名称的方法。

---
+++ src/main/java/com/r/chat/utils/AvatarOwner.java
@@ -0,0 +1,22 @@
+package com.r.chat.utils;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface AvatarOwner {
+    /**
+     * 获取标识图片文件的名称
+     */
+    String getIdentityName();
+
+    // 下面的两个方法正好对应有avatarFile和avatarCover属性的类的get方法，实现类中加了lombok注解就不需要手动实现
+
+    /**
+     * 获取图片文件
+     */
+    MultipartFile getAvatarFile();
+
+    /**
+     * 获取压缩过的图片文件
+     */
+    MultipartFile getAvatarCover();
+}
