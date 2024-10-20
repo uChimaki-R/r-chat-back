91c0d766673f048f5c46400bd503952c04ba9fc4
Crestimes <1259864733@qq.com>
2024-10-20 15:25:17 +0800

    fix: 重新设置redis序列化和反序列化的方式，设置反序列化时遇到未知属性不抛出异常，解决实现了AvatarOwner接口的类中的getIdentityName方法被识别为存在identityName属性导致的反序列化找不到该属性的错误。

--- src/main/java/com/r/chat/config/RedisConfig.java
+++ src/main/java/com/r/chat/config/RedisConfig.java
@@ -1,23 +1,46 @@
 package com.r.chat.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-public class RedisConfig<V> {
+public class RedisConfig {
     @Bean
-    public RedisTemplate<String, V> redisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, V> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
-        // key和value的序列化方式
-        template.setKeySerializer(RedisSerializer.string());
-        template.setValueSerializer(RedisSerializer.json());
-        // hash的key和value的序列化方式
-        template.setHashKeySerializer(RedisSerializer.string());
-        template.setHashValueSerializer(RedisSerializer.json());
-        return template;
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        // 使用jackson来进行值序列化
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        // 配置信息
+        ObjectMapper om = new ObjectMapper();
+        // 所有属性可见
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 设置在反序列化时遇到未知属性不抛出异常
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        // 激活默认的类型解析，允许Jackson处理非最终类的对象。
+        om.activateDefaultTyping(om.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        // 键用string序列化
+        RedisSerializer<?> stringSerializer = new StringRedisSerializer();
+
+        // 配置redisTemplate
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        // 连接工厂，用于连接redis服务器
+        redisTemplate.setConnectionFactory(factory);
+        redisTemplate.setKeySerializer(stringSerializer);  // key序列化
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);  // value序列化
+        redisTemplate.setHashKeySerializer(stringSerializer);  // Hash key序列化
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);  // Hash value序列化
+        // 调用afterPropertiesSet来初始化RedisTemplate，确保所有的属性都已经被设置
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
     }
 }
