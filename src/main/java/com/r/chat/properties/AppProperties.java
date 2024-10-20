b3af82e0f97e1c65ab03edf29e6f2baf7210f857
Crestimes <1259864733@qq.com>
2024-10-20 14:17:11 +0800

    refactor: 将projectFolder属性设置为静态属性，便于在工具类中使用配置信息。 feat: 新增idLength静态属性配置用户id及群聊id长度。新增tokenRandomCharsLength静态属性配置token中拼接的随机字符串的长度。为各静态属性添加对应的set方法。 docs: 添加部分注释。

--- src/main/java/com/r/chat/properties/AppProperties.java
+++ src/main/java/com/r/chat/properties/AppProperties.java
@@ -10,10 +10,42 @@ import java.util.List;
 @ConfigurationProperties(prefix = "r.chat")
 @Data
 public class AppProperties {
-    // ws端口
+    /**
+     * ws端口
+     */
     private Integer wsPort;
-    // 管理员邮箱
+
+    /**
+     * 管理员邮箱
+     */
     private List<String> adminUserIds;
-    // 项目文件夹
-    private String projectFolder;
+
+    // 下面的属性需要在工具类中使用, 所以改为静态属性, 需要使用set方法初始化
+
+    /**
+     * 项目文件夹
+     */
+    public static String projectFolder;
+
+    public void setProjectFolder(String projectFolder) {
+        AppProperties.projectFolder = projectFolder;
+    }
+
+    /**
+     * 用户id及群聊id长度
+     */
+    public static Integer idLength;
+
+    public void setIdLength(Integer idLength) {
+        AppProperties.idLength = idLength;
+    }
+
+    /**
+     * token中拼接的随机字符串的长度
+     */
+    public static Integer tokenRandomCharsLength;
+
+    public void setTokenRandomCharsLength(Integer tokenRandomCharsLength) {
+        AppProperties.tokenRandomCharsLength = tokenRandomCharsLength;
+    }
 }
