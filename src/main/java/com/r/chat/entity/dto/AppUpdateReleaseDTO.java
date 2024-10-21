91f3f1d160b2f210e0efdf78c9ea2bbf4aeb145a
Crestimes <1259864733@qq.com>
2024-10-21 13:50:48 +0800

    feat: 新增app更新发布DTO类

---
+++ src/main/java/com/r/chat/entity/dto/AppUpdateReleaseDTO.java
@@ -0,0 +1,36 @@
+package com.r.chat.entity.dto;
+
+import com.r.chat.entity.constants.Constants;
+import com.r.chat.entity.enums.AppUpdateStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppUpdateReleaseDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 自增id
+     */
+    @NotNull(message = Constants.VALIDATE_EMPTY_ID)
+    private Integer id;
+
+    /**
+     * 发布类型 灰度/全网
+     */
+    @NotNull(message = Constants.VALIDATE_EMPTY_STATUS)
+    private AppUpdateStatusEnum status;
+
+    /**
+     * 灰度用户名单
+     */
+    private String grayscaleIds;
+
+}
