0d9b262f1729563ac95e9333170da2ea9853657c
Crestimes <1259864733@qq.com>
2024-10-21 13:52:20 +0800

    feat: 新增releaseUpdate接口方法用于发布app更新。

--- src/main/java/com/r/chat/service/IAppUpdateService.java
+++ src/main/java/com/r/chat/service/IAppUpdateService.java
@@ -1,6 +1,7 @@
 package com.r.chat.service;
 
 import com.r.chat.entity.dto.AppUpdateDTO;
+import com.r.chat.entity.dto.AppUpdateReleaseDTO;
 import com.r.chat.entity.po.AppUpdate;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -18,4 +19,9 @@ public interface IAppUpdateService extends IService<AppUpdate> {
      * 新增或更新app更新信息
      */
     void saveOrUpdateAppUpdate(AppUpdateDTO appUpdateDTO);
+
+    /**
+     * 发布app更新
+     */
+    void releaseUpdate(AppUpdateReleaseDTO appUpdateReleaseDTO);
 }
