24f52bc19378dabf9b9ac46a5d443f5f3dfa5521
Crestimes <1259864733@qq.com>
2024-10-21 14:55:20 +0800

    feat: 抽取AdminController中系统设置相关的接口，独立为AdminSettingController

---
+++ src/main/java/com/r/chat/controller/AdminSettingController.java
@@ -0,0 +1,48 @@
+package com.r.chat.controller;
+
+import com.r.chat.entity.dto.SysSettingDTO;
+import com.r.chat.entity.result.Result;
+import com.r.chat.entity.vo.SysSettingVO;
+import com.r.chat.redis.RedisUtils;
+import com.r.chat.utils.CopyUtils;
+import com.r.chat.utils.FileUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminSettingController {
+    private final RedisUtils redisUtils;
+
+    /**
+     * 获取系统设置
+     */
+    @GetMapping("/getSysSetting")
+    public Result<SysSettingVO> getSysSetting() {
+        SysSettingVO sysSettingVO = CopyUtils.copyBean(redisUtils.getSysSetting(), SysSettingVO.class);
+        log.info("获取系统设置 {}", sysSettingVO);
+        return Result.success(sysSettingVO);
+    }
+
+    /**
+     * 保存系统设置
+     */
+    @PutMapping("/saveSysSetting")
+    public Result<String> saveSysSetting(SysSettingDTO sysSettingDTO) {
+        log.info("保存系统设置 {}", sysSettingDTO);
+        // 保存机器人头像文件
+        FileUtils.saveAvatarFile(sysSettingDTO);
+        // 保存配置缓存
+        redisUtils.setSysSetting(sysSettingDTO);
+        return Result.success();
+    }
+
+}
