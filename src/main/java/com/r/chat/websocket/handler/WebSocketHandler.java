bc8b369f48ae5ad83b9b46f37cb896b08c08c254
Crestimes <1259864733@qq.com>
2024-10-22 10:37:26 +0800

    feat: 新增ws连接handler，监听ws握手结束后的事件，通过解析url获取token信息并与redis上的token信息进行比对获取对应的用户id。

---
+++ src/main/java/com/r/chat/websocket/handler/WebSocketHandler.java
@@ -0,0 +1,62 @@
+package com.r.chat.websocket.handler;
+
+import com.r.chat.redis.RedisUtils;
+import com.r.chat.utils.URLUtils;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@ChannelHandler.Sharable
+@Component
+@RequiredArgsConstructor
+public class WebSocketHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    private final RedisUtils redisUtils;
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
+        log.info("收到消息: {}", textWebSocketFrame.text());
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        log.info("有新的连接加入");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        log.info("有连接断开");
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        // ws连接成功的时候需要将userId和channel绑定（设置到附件中）
+        if (evt instanceof WebSocketServerProtocolHandler.HandshakeComplete) {
+            // 握手结束后
+            WebSocketServerProtocolHandler.HandshakeComplete handshakeComplete = (WebSocketServerProtocolHandler.HandshakeComplete) evt;
+            // 获取连接uri, 从uri中获取token信息
+            String uri = handshakeComplete.requestUri();
+            log.info("建立ws连接 uri: {}", uri);
+            String token = URLUtils.getParamsByKey(uri, "token");
+            if (token == null) {
+                log.warn("ws断开连接: 未携带token");
+                ctx.close();
+                return;
+            }
+            // 获取userId
+            String userId = redisUtils.getUserIdByToken(token);
+            if (userId == null) {
+                log.warn("ws断开: token不合法");
+                ctx.close();
+                return;
+            }
+            log.info("ws连接成功 userId: {}", userId);
+            // 将userId和channel绑定
+        }
+    }
+}
