c8c869d6fa2731378ccbfe456e1ddbba9e3ec06e
Crestimes <1259864733@qq.com>
2024-10-22 10:29:42 +0800

    feat: 新增getFile静态方法用于获取文件。添加getExeFile静态方法用于根据传入的app版本获取对应的exe文件。 fix: 修正saveFile方法内部代码逻辑。

--- src/main/java/com/r/chat/utils/FileUtils.java
+++ src/main/java/com/r/chat/utils/FileUtils.java
@@ -1,6 +1,7 @@
 package com.r.chat.utils;
 
 import com.r.chat.entity.constants.Constants;
+import com.r.chat.exception.FileNotExistException;
 import com.r.chat.exception.FileSaveFailedException;
 import com.r.chat.properties.AppProperties;
 import lombok.extern.slf4j.Slf4j;
@@ -52,6 +53,22 @@ public class FileUtils {
     }
 
     /**
+     * 根据传入的app版本获取对应的exe文件
+     */
+    public static File getExeFile(String version) {
+        if (version == null) {
+            log.warn("尝试获取文件, 但提供的文件夹为null");
+            return null;
+        }
+        return getFile(
+                AppProperties.projectFolder,
+                Constants.FILE_FOLDER_EXE,
+                version.replace(".", "_"),
+                Constants.FILE_SUFFIX_EXE
+        );
+    }
+
+    /**
      * 保存文件
      * @param rootPath 根目录
      * @param folder 文件夹，不存在的话会创建
@@ -67,6 +84,7 @@ public class FileUtils {
                 log.info("创建目录成功: {}", targetFolder.getAbsolutePath());
             } else {
                 log.warn("创建目录失败: {}", targetFolder.getAbsolutePath());
+                throw new FileSaveFailedException(Constants.MESSAGE_FAILED_TO_CREATE_FOLDER);
             }
         }
         try {
@@ -78,5 +96,19 @@ public class FileUtils {
             log.error("保存文件失败: {}", e.getMessage());
             throw new FileSaveFailedException(Constants.MESSAGE_FAILED_TO_SAVE_FILE);
         }
+    }
+
+    /**
+     * 获取文件
+     */
+    public static File getFile(String rootPath, String folder, String fileName, String suffix) {
+        File targetFolder = new File(rootPath, folder);
+        File localFile = new File(targetFolder, fileName + suffix);
+        if (!localFile.exists()) {
+            log.error("打开文件失败: 文件不存在 {}", localFile.getAbsolutePath());
+            throw new FileNotExistException(Constants.MESSAGE_FILE_NOT_EXIST);
+        }
+        log.info("获取到文件 {}", localFile);
+        return localFile;
     }
 }
