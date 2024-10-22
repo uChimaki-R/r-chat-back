9301ee8b92f49ad7000c2f3f8ef12a52a9c86dc6
Crestimes <1259864733@qq.com>
2024-10-22 11:46:53 +0800

    feat: 使用ChannelUtils获取channel对应的userId，并在日志信息中显示。 refactor: 修改部分日志输出内容和日志输出等级。

--- src/main/java/com/r/chat/websocket/handler/HeartbeatHandler.java
+++ src/main/java/com/r/chat/websocket/handler/HeartbeatHandler.java
@@ -1,26 +1,30 @@
 package com.r.chat.websocket.handler;
 
+import com.r.chat.websocket.utils.ChannelUtils;
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.timeout.IdleStateEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @ChannelHandler.Sharable
 @Component
+@RequiredArgsConstructor
 public class HeartbeatHandler extends ChannelDuplexHandler {
+
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
         if (evt instanceof IdleStateEvent) {
             IdleStateEvent event = (IdleStateEvent) evt;
             switch (event.state()) {
                 case READER_IDLE:
-                    log.info("客户端心跳间隔过长, 关闭连接");
+                    log.debug("{} 心跳间隔过长, 关闭连接", ChannelUtils.getUserId(ctx.channel()));
                     ctx.close();
                 case WRITER_IDLE:
-                    log.info("到达发送心跳间隔, 向客户端发送心跳");
+                    log.debug("到达发送心跳间隔, 向 {} 发送心跳", ChannelUtils.getUserId(ctx.channel()));
                     ctx.writeAndFlush("heartbeat");
                     break;
             }
