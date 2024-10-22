ea7be2a8612f664ae29bd4f62f3cb1ba94d2ffb8
Crestimes <1259864733@qq.com>
2024-10-22 11:45:53 +0800

    feat: 新增ChannelUtils工具类。添加USER_CHANNEL_MAP、GROUP_CHANNEL_MAP静态属性管理用户channel和群聊channelGroup。添加addContext静态方法用于双向绑定channel<->userId，重载版本可以同时将用户的channel加入到groupId对应的channelGroup中。添加getUserId静态方法用于获取channel绑定的userId。

---
+++ src/main/java/com/r/chat/websocket/utils/ChannelUtils.java
@@ -0,0 +1,71 @@
+package com.r.chat.websocket.utils;
+
+import io.netty.channel.Channel;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.AttributeKey;
+import io.netty.util.concurrent.GlobalEventExecutor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Component
+@RequiredArgsConstructor
+public class ChannelUtils {
+    /**
+     * userId->channel
+     */
+    private static final ConcurrentMap<String, Channel> USER_CHANNEL_MAP = new ConcurrentHashMap<>();
+
+    /**
+     * groupId->channel
+     */
+    private static final ConcurrentMap<String, ChannelGroup> GROUP_CHANNEL_MAP = new ConcurrentHashMap<>();
+
+
+    /**
+     * 双向绑定channel<->userId
+     */
+    public static void addContext(String userId, Channel channel) {
+        // 由于channel序列化之后后续无法使用，所以无法保存到redis中，只能直接保存到内存中
+        // 这里使用附件绑定userId（channel->userId），使用线程安全的map通过userId找到channel（userId->channel）
+        // （理论上channel->userId也可以用一个channelId到userId的map来保存，但是不够优雅）
+        // 获取一个名为channelId的key
+        String channelId = channel.id().asLongText();
+        AttributeKey<String> key;
+        // 为了线程安全的操作，需要先判断是否创建过该key
+        if (!AttributeKey.exists(channelId)) {
+            key = AttributeKey.newInstance(channelId);
+        } else {
+            key = AttributeKey.valueOf(channelId);
+        }
+        // 将这个名为channelId的key对应的value设置为userId（channel->userId）
+        channel.attr(key).set(userId);
+        // 保存到map（userId->channel）
+        USER_CHANNEL_MAP.put(userId, channel);
+    }
+
+    /**
+     * 双向绑定channel<->userId，并将用户的channel加入到groupId对应的channelGroup中
+     */
+    public static void addContext(String userId, String groupId, Channel channel) {
+        addContext(userId, channel);
+        // 添加进群聊channelGroup
+        ChannelGroup channelGroup = GROUP_CHANNEL_MAP.get(groupId);
+        if (channelGroup == null) {
+            channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+            GROUP_CHANNEL_MAP.put(groupId, channelGroup);
+        }
+        // 将用户的channel加入到群聊channelGroup中
+        channelGroup.add(channel);
+    }
+
+    /**
+     * 获取channel绑定的userId
+     */
+    public static String getUserId(Channel channel) {
+        return (String) channel.attr(AttributeKey.valueOf(channel.id().asLongText())).get();
+    }
+}
