6a220ecbcff8dbd1ce1e3ab8d049f0e7d5c919ff
Crestimes <1259864733@qq.com>
2024-10-24 10:16:20 +0800

    feat: 新增selectChatSessionUserVOList接口方法用于根据用户id获取该用户的全部会话信息（需要和chat_session表联查获取具体信息，如果是群聊，还要和user_contact表联查获取该群聊的群成员数量）

--- src/main/java/com/r/chat/mapper/ChatSessionUserMapper.java
+++ src/main/java/com/r/chat/mapper/ChatSessionUserMapper.java
@@ -2,6 +2,9 @@ package com.r.chat.mapper;
 
 import com.r.chat.entity.po.ChatSessionUser;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.r.chat.entity.vo.ChatSessionUserVO;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface ChatSessionUserMapper extends BaseMapper<ChatSessionUser> {
 
+    /**
+     * 根据用户id获取该用户的全部会话信息，需要和chat_session表联查获取具体信息
+     * 如果是群聊，还要和user_contact表联查获取该群聊的群成员数量
+     */
+    List<ChatSessionUserVO> selectChatSessionUserVOList(String userId);
 }
