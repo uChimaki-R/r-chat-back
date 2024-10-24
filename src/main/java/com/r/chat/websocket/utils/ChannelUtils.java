df1c3eefafa065d6acc1dfee2f00e57bfd31849f
Crestimes <1259864733@qq.com>
2024-10-24 14:46:37 +0800

    feat: 完善initChannel方法，新增添加用户心跳缓存和获取用户所有会话消息的相关代码逻辑。新增部分日志输出内容。新增removeChannel方法，在连接断开时调用，用于移除用户channel、移除用户心跳缓存和更新用户最后离线时间。 fix: 修正sendMessage2User、sendMessage2Group方法内部代码逻辑。

--- src/main/java/com/r/chat/websocket/utils/ChannelUtils.java
+++ src/main/java/com/r/chat/websocket/utils/ChannelUtils.java
@@ -1,11 +1,18 @@
 package com.r.chat.websocket.utils;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.r.chat.entity.enums.IdPrefixEnum;
 import com.r.chat.entity.enums.UserContactTypeEnum;
 import com.r.chat.entity.po.UserInfo;
+import com.r.chat.entity.result.Message;
+import com.r.chat.entity.vo.ChatSessionUserVO;
+import com.r.chat.entity.message.WsInitMessage;
+import com.r.chat.mapper.ChatSessionUserMapper;
 import com.r.chat.mapper.UserInfoMapper;
+import com.r.chat.properties.AppProperties;
 import com.r.chat.redis.RedisUtils;
+import com.r.chat.utils.JsonUtils;
 import io.netty.channel.Channel;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
@@ -16,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
+import java.time.Duration;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
@@ -32,19 +39,23 @@ public class ChannelUtils {
     private static final ConcurrentMap<String, Channel> USER_CHANNEL_MAP = new ConcurrentHashMap<>();
 
     /**
-     * groupId->channel
+     * groupId->channelGroup
      */
     private static final ConcurrentMap<String, ChannelGroup> GROUP_CHANNEL_MAP = new ConcurrentHashMap<>();
 
     private final RedisUtils redisUtils;
     private final UserInfoMapper userInfoMapper;
+    private final AppProperties appProperties;
+    private final ChatSessionUserMapper chatSessionUserMapper;
     // 工具类的方法却不使用静态的原因: 需要用到其他bean对象（redisUtils、mapper等），静态注入比较麻烦，把工具类也交给IOC管理，要用再注入就行了（因为也只有netty用，不算麻烦）
 
     /**
      * 在有效连接建立时调用
      * 1. 双向绑定channel<->userId
      * 2. 将用户的channel加入到用户加入的群聊对应的channelGroup中
-     * 3. 更新用户最后登陆时间
+     * 3. 添加用户心跳缓存
+     * 4. 更新用户最后登陆时间
+     * 5. 获取用户所有会话消息、上次下线后的未读聊天信息、好友申请数量
      */
     public void initChannel(String userId, Channel channel) {
         // 由于channel序列化之后后续无法使用，所以无法保存到redis中，只能直接保存到内存中
@@ -63,6 +74,7 @@ public class ChannelUtils {
         channel.attr(key).set(userId);
         // 保存到map（userId->channel）
         USER_CHANNEL_MAP.put(userId, channel);
+        log.info("绑定channel {}", channel);
         // 将用户的channel加入到用户加入的群聊对应的channelGroup中
         // 从redis中获取用户的联系人id列表
         List<String> userContactIds = redisUtils.getUserContactIds(userId);
@@ -73,12 +85,62 @@ public class ChannelUtils {
                 add2Group(contactId, channel);
             }
         }
+
+        // 添加用户心跳缓存
+        redisUtils.setUserHeartBeat(userId);
+
         // 更新用户最后登陆时间
         UpdateWrapper<UserInfo> updateWrapper = new UpdateWrapper<>();
+        Long lastLoginTime = System.currentTimeMillis();
         updateWrapper.lambda()
                 .eq(UserInfo::getUserId, userId)
-                .set(UserInfo::getLastLoginTime, LocalDateTime.now());
+                .set(UserInfo::getLastLoginTime, lastLoginTime);
         userInfoMapper.update(null, updateWrapper);
+        log.info("更新用户最后登陆时间 lastLoginTime: {}", lastLoginTime);
+
+        // 获取用户上次下线后的未读聊天信息
+        // 从配置里找能够从服务端获取的最长未读聊天信息的天数，如果上次离线时间超过了这个时间，也只能获取配置的最大天数前的信息
+        QueryWrapper<UserInfo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.lambda().eq(UserInfo::getUserId, userId);
+        UserInfo userInfo = userInfoMapper.selectOne(queryWrapper);
+        Long fromTime = userInfo.getLastOffTime();
+        long now = System.currentTimeMillis();
+        if (now - fromTime >= Duration.ofDays(appProperties.getMaxUnreadChatFetchDays()).toMillis()) {
+            fromTime = now - Duration.ofDays(appProperties.getMaxUnreadChatFetchDays()).toMillis();
+        }
+
+        // 获取用户所有会话消息
+        List<ChatSessionUserVO> chatSessionUserVOList = chatSessionUserMapper.selectChatSessionUserVOList(userId);
+        log.info("获取所有会话消息 {}", chatSessionUserVOList);
+
+        // 发送ws初始化消息
+        WsInitMessage wsInitMessage = new WsInitMessage();
+        wsInitMessage.setChatSessionUserList(chatSessionUserVOList);
+        log.info("发送ws初始化消息 {}", wsInitMessage);
+        sendMessage2User(userId, userId, wsInitMessage);
+    }
+
+    /**
+     * 连接断开时调用
+     * 1. 移除用户channel
+     * 2. 移除用户心跳缓存
+     * 3. 更新用户最后离线时间
+     */
+    public void removeChannel(Channel channel) {
+        // 移除用户channel
+        String userId = getUserId(channel);
+        USER_CHANNEL_MAP.remove(userId);
+        log.info("移除绑定 channel: {}", channel);
+        // 移除心跳缓存
+        redisUtils.removeUserHeartBeat(userId);
+        // 更新用户最后离线时间
+        UpdateWrapper<UserInfo> updateWrapper = new UpdateWrapper<>();
+        Long lastOffTime = System.currentTimeMillis();
+        updateWrapper.lambda()
+                .eq(UserInfo::getUserId, userId)
+                .set(UserInfo::getLastOffTime, lastOffTime);
+        userInfoMapper.update(null, updateWrapper);
+        log.info("更新用户最后离线时间 lastOffTime: {}", lastOffTime);
     }
 
     /**
@@ -93,6 +155,7 @@ public class ChannelUtils {
         }
         // 将用户的channel加入到群聊channelGroup中
         channelGroup.add(channel);
+        log.info("添加channel到groupId对应的channelGroup groupId: {}, channelGroup: {}", groupId, channelGroup);
     }
 
     /**
@@ -105,13 +168,16 @@ public class ChannelUtils {
     /**
      * 发送消息给用户
      */
-    public void sendMessage2User(String userId, String message) {
-        Channel channel = USER_CHANNEL_MAP.get(userId);
+    public void sendMessage2User(String from, String to, Message message) {
+        message.setSendUserId(from);
+        message.setContactId(to);
+        message.setSendTime(System.currentTimeMillis());
+        Channel channel = USER_CHANNEL_MAP.get(to);
         if (channel == null) {
             return;
         }
-        log.info("发送消息给用户 {}, message: {}", userId, message);
-        channel.writeAndFlush(new TextWebSocketFrame(message));
+        channel.writeAndFlush(new TextWebSocketFrame(JsonUtils.obj2Json(message)));
+        log.info("{} 发送消息给 {}, message: {}", from, to, message);
     }
 
     /**
@@ -122,7 +188,7 @@ public class ChannelUtils {
         if (group == null) {
             return;
         }
-        log.info("发送消息到群聊 {}, message: {}", groupId, message);
         group.writeAndFlush(new TextWebSocketFrame(message));
+        log.info("发送消息到群聊 {}, message: {}", groupId, message);
     }
 }
