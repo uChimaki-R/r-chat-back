0768b3d2dd835a91d06a4f0466ea4fcf257f6c28
Crestimes <1259864733@qq.com>
2024-10-24 14:31:43 +0800

    feat: 新增会话信息VO类

---
+++ src/main/java/com/r/chat/entity/vo/ChatSessionUserVO.java
@@ -0,0 +1,50 @@
+package com.r.chat.entity.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatSessionUserVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 用户id
+     */
+    private String userId;
+
+    /**
+     * 联系人id
+     */
+    private String contactId;
+
+    /**
+     * 会话id
+     */
+    private String sessionId;
+
+    /**
+     * 联系人名称
+     */
+    private String contactName;
+
+    /**
+     * 最后接收的消息
+     */
+    private String lastMessage;
+
+    /**
+     * 最后接收消息的时间
+     */
+    private Long lastReceiveTime;
+
+    /**
+     * 群聊成员数量（前端在会话界面需要群聊人数，从这里获取）用户会话的话值为0
+     */
+    private Integer memberCount;
+}
