c7a48850762ce248599c1e268ec946c7d8da64e1
Crestimes <1259864733@qq.com>
2024-10-24 10:38:45 +0800

    feat: 新增ws初始化消息类

---
+++ src/main/java/com/r/chat/entity/message/WsInitMessage.java
@@ -0,0 +1,36 @@
+package com.r.chat.entity.message;
+
+import com.r.chat.entity.enums.MessageTypeEnum;
+import com.r.chat.entity.po.ChatMessage;
+import com.r.chat.entity.result.Message;
+import com.r.chat.entity.vo.ChatSessionUserVO;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@Setter
+public class WsInitMessage extends Message implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public WsInitMessage(){
+        super(MessageTypeEnum.WS_INIT);
+    }
+
+    /**
+     * 会话信息列表
+     */
+    private List<ChatSessionUserVO> chatSessionUserList;
+
+    /**
+     * 聊天消息列表
+     */
+    private List<ChatMessage> chatMessageList;
+
+    /**
+     * 好友申请数量
+     */
+    private Integer applyCount;
+}
