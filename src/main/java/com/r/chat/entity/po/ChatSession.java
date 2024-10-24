1c3f414a7cfa0067d9de7f7f81b1eadeab394e8e
Crestimes <1259864733@qq.com>
2024-10-24 10:15:39 +0800

    refactor: 修改lastReceiveTime属性类型[LocalDateTime->Long]

--- src/main/java/com/r/chat/entity/po/ChatSession.java
+++ src/main/java/com/r/chat/entity/po/ChatSession.java
@@ -1,17 +1,14 @@
 package com.r.chat.entity.po;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
-import java.time.LocalDateTime;
-import com.baomidou.mybatisplus.annotation.TableId;
-import java.io.Serializable;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
-import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
 
 /**
  * <p>
@@ -44,10 +41,6 @@ public class ChatSession implements Serializable {
     /**
      * 最后接收消息的时间
      */
-    @TableField("last_receive_time")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime lastReceiveTime;
-
+    private Long lastReceiveTime;
 
 }
