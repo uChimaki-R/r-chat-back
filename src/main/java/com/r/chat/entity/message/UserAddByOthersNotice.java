1c07a0b9e3956605f1e722653e1be2358a5081ef
Crestimes <1259864733@qq.com>
2024-10-25 21:36:49 +0800

    feat: 新增对方同意了自己的申请的通知，收到该通知后前端会使用chatSessionUserVO属性中的数据渲染会话

---
+++ src/main/java/com/r/chat/entity/message/UserAddByOthersNotice.java
@@ -0,0 +1,27 @@
+package com.r.chat.entity.message;
+
+import com.r.chat.entity.enums.NoticeTypeEnum;
+import com.r.chat.entity.result.Notice;
+import com.r.chat.entity.vo.ChatSessionUserVO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * 对方同意了自己的申请，二者成为了好友的通知，让前端渲染会话
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class UserAddByOthersNotice extends Notice implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public UserAddByOthersNotice() {
+        super(NoticeTypeEnum.USER_ADD_BY_OTHERS);
+    }
+
+    /**
+     * 用于渲染会话框的内容，lastMessage就是申请信息
+     */
+    private ChatSessionUserVO chatSessionUserVO;
+}
