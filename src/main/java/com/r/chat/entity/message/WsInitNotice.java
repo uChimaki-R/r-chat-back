bc08fc0fe20781398af8a0118b17fe9b0e23e620
Crestimes <1259864733@qq.com>
2024-10-25 21:33:39 +0800

    refactor: 重命名[WsInitMessage->WsInitNotice、Message->Notice、MessageTypeEnum->NoticeTypeEnum]。 docs: 添加部分注释信息。

--- src/main/java/com/r/chat/entity/message/WsInitNotice.java
+++ src/main/java/com/r/chat/entity/message/WsInitNotice.java
@@ -1,8 +1,8 @@
 package com.r.chat.entity.message;
 
-import com.r.chat.entity.enums.MessageTypeEnum;
+import com.r.chat.entity.enums.NoticeTypeEnum;
 import com.r.chat.entity.po.ChatMessage;
-import com.r.chat.entity.result.Message;
+import com.r.chat.entity.result.Notice;
 import com.r.chat.entity.vo.ChatSessionUserVO;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -10,13 +10,16 @@ import lombok.EqualsAndHashCode;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * 初始化ws连接后的通知，把会话信息、聊天信息、好友申请数量带到前端，让前端渲染界面
+ */
 @EqualsAndHashCode(callSuper = true)
 @Data
-public class WsInitMessage extends Message implements Serializable {
+public class WsInitNotice extends Notice implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    public WsInitMessage(){
-        super(MessageTypeEnum.WS_INIT);
+    public WsInitNotice(){
+        super(NoticeTypeEnum.WS_INIT);
     }
 
     /**
