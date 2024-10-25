f29ee7b373ea6682225fccb074a901551dbeafd9
Crestimes <1259864733@qq.com>
2024-10-25 21:25:24 +0800

    fix: MessageTypeEnum应该是用户之间发送的信息的类型，而不是ws发送的通知类型，删去非用户信息类型的枚举，去除无用的属性message

--- src/main/java/com/r/chat/entity/enums/MessageTypeEnum.java
+++ src/main/java/com/r/chat/entity/enums/MessageTypeEnum.java
@@ -5,28 +5,16 @@ import lombok.Getter;
 
 @Getter
 public enum MessageTypeEnum {
-    WS_INIT(0, "", "连接ws"),
-    CHAT(1, "", "普通聊天消息"),
-    USER_ADD(2, "", "添加好友打招呼"),
-    USER_LEAVE_GROUP(3, "%s退出了群聊", "退出群聊"),
-    USER_IS_REMOVED_GROUP(4, "%s被群主移出了群聊", "被群主移出了群聊"),
-    GROUP_CREATE(5, "群聊已经创建好，可以和好友一起畅聊了", "群创建成功"),
-    GROUP_ADD(6, "%s加入了群聊", "加入群聊"),
-    GROUP_RENAME(7, "", "更新群聊名"),
-    GROUP_DISSOLUTION(8, "群聊已解散", "解散群聊"),
-    CONTACT_APPLY(9, "", "好友申请"),
-    FILE_UPLOAD(10, "", "媒体文件"),
-    FILE_UPLOAD_COMPLETED(11, "", "文件上传完毕"),
-    FORCE_OFF_LINE(12, "", "强制下线");
+    CHAT(0, "普通聊天消息"),
+
+    FILE_UPLOAD(1, "媒体文件"),;
 
     @EnumValue
     private final Integer value;
-    private final String message;
     private final String desc;
 
-    MessageTypeEnum(Integer value, String message, String desc) {
+    MessageTypeEnum(Integer value, String desc) {
         this.value = value;
-        this.message = message;
         this.desc = desc;
     }
 }
