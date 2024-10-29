d5c484cfdc178a3fab87bf4a31d17bec7bb11eea
Crestimes <1259864733@qq.com>
2024-10-29 13:53:13 +0800

    feat: 启用复合主键功能

--- src/main/java/com/r/chat/RChatApplication.java
+++ src/main/java/com/r/chat/RChatApplication.java
@@ -1,5 +1,6 @@
 package com.r.chat;
 
+import com.github.jeffreyning.mybatisplus.conf.EnableMPP;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan(basePackages = {"com.r.chat.mapper"})
 @SpringBootApplication(scanBasePackages = {"com.r.chat"})
 @Order(1)
+@EnableMPP
 public class RChatApplication {
     public static void main(String[] args) {
         SpringApplication.run(RChatApplication.class, args);
