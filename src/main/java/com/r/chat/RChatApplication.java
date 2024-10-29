7ac5c69b67d3dfa257262e86e3122d618af645cb
Crestimes <1259864733@qq.com>
2024-10-29 11:29:07 +0800

    feat: 设置启动顺序为1

--- src/main/java/com/r/chat/RChatApplication.java
+++ src/main/java/com/r/chat/RChatApplication.java
@@ -3,6 +3,7 @@ package com.r.chat;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAsync
 @MapperScan(basePackages = {"com.r.chat.mapper"})
 @SpringBootApplication(scanBasePackages = {"com.r.chat"})
+@Order(1)
 public class RChatApplication {
     public static void main(String[] args) {
         SpringApplication.run(RChatApplication.class, args);
