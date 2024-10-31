0ad0de71dd9f5f85bc448142cd60b71d4e63e52d
Crestimes <1259864733@qq.com>
2024-10-31 10:41:47 +0800

    feat: 新增联系人名称修改的通知类

---
+++ src/main/java/com/r/chat/entity/message/ContactRenameNotice.java
@@ -0,0 +1,31 @@
+package com.r.chat.entity.message;
+
+import com.r.chat.entity.enums.NoticeTypeEnum;
+import com.r.chat.entity.result.Notice;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * 联系人名称修改通知，可以是群聊名称修改了，也可以是用户名称修改了
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ContactRenameNotice extends Notice implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public ContactRenameNotice() {
+        super(NoticeTypeEnum.CONTACT_RENAME);
+    }
+
+    /**
+     * 需要更新名称的联系人id
+     */
+    private String contactId;
+
+    /**
+     * 更新的名称
+     */
+    private String contactName;
+}
