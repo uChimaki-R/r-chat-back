9257e88b8ffa9f538910adbe2dd0e45d734ac7a2
Crestimes <1259864733@qq.com>
2024-10-31 21:18:50 +0800

    feat: 新增处理聊天信息相关请求的controller。新增sendMessage方法用于处理用户发送信息的请求。

---
+++ src/main/java/com/r/chat/controller/ChatController.java
@@ -0,0 +1,35 @@
+package com.r.chat.controller;
+
+import com.r.chat.entity.dto.ChatMessageDTO;
+import com.r.chat.entity.result.Result;
+import com.r.chat.entity.vo.ChatMessageVO;
+import com.r.chat.service.IChatMessageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("/chat")
+@RequiredArgsConstructor
+public class ChatController {
+    private final IChatMessageService chatMessageServiceImpl;
+
+    /**
+     * 用户发送消息
+     * @return 返回增加的ChatMessage内容加上lastMessage、lastReceiveTime包装成的ChatMessageVO，让前端更新（messageId由后端产生）
+     */
+    @PostMapping("/sendMessage")
+    public Result<ChatMessageVO> sendMessage(@Valid ChatMessageDTO chatMessageDTO) {
+        log.info("发送聊天信息 {}", chatMessageDTO);
+        ChatMessageVO chatMessage = chatMessageServiceImpl.saveMessage(chatMessageDTO);
+        log.info("返回发送聊天信息后更新的内容 {}", chatMessage);
+        return Result.success(chatMessage);
+    }
+}
