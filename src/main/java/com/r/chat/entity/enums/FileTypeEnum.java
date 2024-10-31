31b0056a076137c79664d2b86a802f5fe7d97f4e
Crestimes <1259864733@qq.com>
2024-10-31 16:28:45 +0800

    feat: 新增文件类型枚举类

---
+++ src/main/java/com/r/chat/entity/enums/FileTypeEnum.java
@@ -0,0 +1,17 @@
+package com.r.chat.entity.enums;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import lombok.Getter;
+
+@Getter
+public enum FileTypeEnum {
+    MEDIA(0, "媒体文件");
+
+    @EnumValue
+    private final int value;
+    private final String desc;
+    FileTypeEnum(int value, String desc) {
+        this.value = value;
+        this.desc = desc;
+    }
+}
