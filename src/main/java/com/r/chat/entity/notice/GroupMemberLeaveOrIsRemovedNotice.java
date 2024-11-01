f3be31efd373982588c647fc7e12b77d21059ad2
Crestimes <1259864733@qq.com>
2024-11-02 00:44:06 +0800

    feat: 新增离开群聊或者被移出群聊的通知类

---
+++ src/main/java/com/r/chat/entity/notice/GroupMemberLeaveOrIsRemovedNotice.java
@@ -0,0 +1,27 @@
+package com.r.chat.entity.notice;
+
+import com.r.chat.entity.enums.NoticeTypeEnum;
+import com.r.chat.entity.result.Notice;
+import com.r.chat.entity.vo.ChatMessageVO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * 离开群聊或者被移出群聊的通知
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class GroupMemberLeaveOrIsRemovedNotice extends Notice implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public GroupMemberLeaveOrIsRemovedNotice() {
+        super(NoticeTypeEnum.GROUP_MEMBER_LEAVE_OR_IS_REMOVED);
+    }
+
+    /**
+     * 发送的消息内容和lastReceiveTime、lastMessage、新的群聊人数等
+     */
+    private ChatMessageVO chatMessageVO;
+}
