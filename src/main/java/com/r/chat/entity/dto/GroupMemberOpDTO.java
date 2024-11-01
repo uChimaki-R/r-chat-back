c164c515bda7021a814d8885ca15c72e7a170e9a
Crestimes <1259864733@qq.com>
2024-11-02 00:42:03 +0800

    feat: 新增群成员操作DTO类

---
+++ src/main/java/com/r/chat/entity/dto/GroupMemberOpDTO.java
@@ -0,0 +1,27 @@
+package com.r.chat.entity.dto;
+
+import com.r.chat.entity.constants.Constants;
+import com.r.chat.entity.enums.GroupMemberOpTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupMemberOpDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @NotEmpty(message = Constants.VALIDATE_EMPTY_GROUP_ID)
+    private String groupId;
+
+    @NotEmpty(message = Constants.VALIDATE_EMPTY_CONTACT_ID)
+    private String contactIds;  // 批量操作
+
+    @NotNull(message = Constants.VALIDATE_EMPTY_OPERATE_TYPE)
+    private GroupMemberOpTypeEnum opType;
+}
