36e299e54da024f0b5e859b1ae0d9727ece741d5
Crestimes <1259864733@qq.com>
2024-11-01 13:52:31 +0800

    feat: 新增群聊已解散的通知类

---
+++ src/main/java/com/r/chat/entity/message/GroupDisbandNotice.java
@@ -0,0 +1,27 @@
+package com.r.chat.entity.message;
+
+import com.r.chat.entity.enums.NoticeTypeEnum;
+import com.r.chat.entity.result.Notice;
+import com.r.chat.entity.vo.ChatMessageVO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * 群聊已解散的通知
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class GroupDisbandNotice extends Notice implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public GroupDisbandNotice() {
+        super(NoticeTypeEnum.GROUP_DISBAND);
+    }
+
+    /**
+     * 发送的消息内容和lastReceiveTime、lastMessage
+     */
+    private ChatMessageVO chatMessageVO;
+}
