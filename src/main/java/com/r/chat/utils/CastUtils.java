bd380409e8a04ff28f7a644c5e5fbbc36205b251
Crestimes <1259864733@qq.com>
2024-11-01 13:46:50 +0800

    refactor: castList方法作用类型从List改为Collection，使之更加通用

--- src/main/java/com/r/chat/utils/CastUtils.java
+++ src/main/java/com/r/chat/utils/CastUtils.java
@@ -1,12 +1,13 @@
 package com.r.chat.utils;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CastUtils {
     public static <T> List<T> castList(Object obj, Class<T> clazz) {
-        if (obj instanceof List<?>) {
-            List<?> list = (List<?>) obj;
+        if (obj instanceof Collection<?>) {
+            Collection<?> list = (Collection<?>) obj;
             return list.stream().map(clazz::cast).collect(Collectors.toList());
         }
         return null;
