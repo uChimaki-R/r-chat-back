353cebd39b937a415ccef47a0eee1362f699eb1e
Crestimes <1259864733@qq.com>
2024-11-01 10:44:09 +0800

    feat: 新增downloadFile方法用于处理下载文件的请求。 style: 格式化代码。

--- src/main/java/com/r/chat/controller/ChatController.java
+++ src/main/java/com/r/chat/controller/ChatController.java
@@ -1,18 +1,27 @@
 package com.r.chat.controller;
 
+import com.r.chat.entity.constants.Constants;
 import com.r.chat.entity.dto.ChatMessageDTO;
+import com.r.chat.entity.dto.FileDownloadDTO;
 import com.r.chat.entity.dto.FileUploadDTO;
 import com.r.chat.entity.result.Result;
 import com.r.chat.entity.vo.ChatMessageVO;
+import com.r.chat.exception.FileDownloadException;
+import com.r.chat.exception.FileNotExistException;
 import com.r.chat.service.IChatMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
 
 @Slf4j
 @Validated
@@ -24,6 +33,7 @@ public class ChatController {
 
     /**
      * 用户发送消息
+     *
      * @return 返回增加的ChatMessage内容加上lastMessage、lastReceiveTime包装成的ChatMessageVO，让前端更新（messageId由后端产生）
      */
     @PostMapping("/sendMessage")
@@ -43,5 +53,39 @@ public class ChatController {
         chatMessageServiceImpl.saveFile(uploadDTO);
         log.info("保存文件成功 {}", uploadDTO);
         return Result.success();
+    }
+
+    /**
+     * 下载文件。可能是头像文件或聊天文件，如果是头像的话文件名是U/G+数字，如果是聊天文件的话文件名则是messageId
+     */
+    @GetMapping("/downloadFile")
+    public void downloadFile(HttpServletResponse response, @Valid FileDownloadDTO downloadDTO) {
+        downloadDTO.setIsCover(downloadDTO.getIsCover() == null || downloadDTO.getIsCover());  // 默认获取缩略图
+        log.info("下载文件 {}", downloadDTO);
+        File file = chatMessageServiceImpl.getFile(downloadDTO);
+        if (file == null) {
+            log.info("获取下载文件失败: 文件不存在 {}", downloadDTO);
+            throw new FileNotExistException(Constants.MESSAGE_FILE_NOT_EXIST);
+        }
+        log.info("获取下载文件成功, 开始下载文件 {}", file);
+        // 下载文件的操作
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"");
+        response.setContentType("application/x-msdownload;charset=UTF-8");
+        response.setHeader("Content-Length", String.valueOf(file.length()));
+        try (
+                OutputStream outputStream = response.getOutputStream();
+                FileInputStream fileInputStream = new FileInputStream(file);
+        ) {
+            byte[] buffer = new byte[1024];  // 缓冲区
+            int len;
+            while ((len = fileInputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, len);
+            }
+            outputStream.flush();
+        } catch (Exception e) {
+            log.warn("文件下载失败 失败原因: {}", e.getCause().getMessage());
+            throw new FileDownloadException(Constants.MESSAGE_INTERNAL_ERROR);
+        }
+        log.info("下载文件成功 {}", downloadDTO);
     }
 }
