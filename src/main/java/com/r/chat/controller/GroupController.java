76c213109556cf80c39a93a760ab1a51f8979cac
Crestimes <1259864733@qq.com>
2024-11-08 22:18:48 +0800

    perf: 重构loadMyGroup、getGroupInfo、getGroupInfo4Chat方法，将业务相关代码移动到业务层中完成。 refactor: 删除部分冗余代码。

--- src/main/java/com/r/chat/controller/GroupController.java
+++ src/main/java/com/r/chat/controller/GroupController.java
@@ -5,17 +5,10 @@ import com.r.chat.entity.constants.Constants;
 import com.r.chat.entity.dto.GroupInfoDTO;
 import com.r.chat.entity.dto.BasicInfoDTO;
 import com.r.chat.entity.dto.GroupMemberOpDTO;
-import com.r.chat.entity.enums.GroupInfoStatusEnum;
-import com.r.chat.entity.enums.UserContactStatusEnum;
-import com.r.chat.entity.po.GroupInfo;
-import com.r.chat.entity.po.UserContact;
 import com.r.chat.entity.vo.GroupInfo4ChatVO;
 import com.r.chat.entity.vo.GroupDetailInfoVO;
 import com.r.chat.entity.result.Result;
 import com.r.chat.entity.vo.BasicInfoVO;
-import com.r.chat.exception.GroupDisbandException;
-import com.r.chat.exception.GroupNotExistException;
-import com.r.chat.exception.IllegalOperationException;
 import com.r.chat.service.IGroupInfoService;
 import com.r.chat.service.IUserContactService;
 import com.r.chat.utils.CopyUtils;
@@ -26,9 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
-import java.util.Collections;
 import java.util.List;
-
 
 @Slf4j
 @Validated
@@ -50,15 +41,12 @@ public class GroupController {
     }
 
     /**
-     * 获取自己创建的群聊
+     * 获取自己创建的群聊的信息
      */
-    @GetMapping("/loadMyGroup")
-    public Result<List<GroupDetailInfoVO>> loadMyGroup() {
-        String ownerId = UserTokenInfoContext.getCurrentUserId();
-        List<GroupInfo> list = groupInfoService.lambdaQuery().eq(GroupInfo::getGroupOwnerId, ownerId).list();
-        List<GroupDetailInfoVO> groupDetailInfoVOList = CopyUtils.copyList(list, GroupDetailInfoVO.class);
-        initGroupMemberCounts(groupDetailInfoVOList);  // 计算群成员数
-        log.info("获取创建的群聊 {}", groupDetailInfoVOList);
+    @GetMapping("/loadMyGroupInfo")
+    public Result<List<GroupDetailInfoVO>> loadMyGroupInfo() {
+        log.info("获取自己创建的群聊的信息");
+        List<GroupDetailInfoVO> groupDetailInfoVOList = groupInfoService.loadMyGroupInfo();
         return Result.success(groupDetailInfoVOList);
     }
 
@@ -67,10 +55,8 @@ public class GroupController {
      */
     @GetMapping("/getGroupInfo")
     public Result<GroupDetailInfoVO> getGroupInfo(@NotEmpty(message = Constants.VALIDATE_EMPTY_GROUP_ID) String groupId) {
-        GroupInfo groupInfo = getBasicGroupInfo(groupId);
-        GroupDetailInfoVO groupDetailInfoVO = CopyUtils.copyBean(groupInfo, GroupDetailInfoVO.class);
-        initGroupMemberCounts(groupDetailInfoVO);  // 计算群成员数
-        log.info("获取群聊简介部分的详情 {}", groupDetailInfoVO);
+        log.info("获取群聊简介部分的详情 groupId: {}", groupId);
+        GroupDetailInfoVO groupDetailInfoVO = groupInfoService.getGroupDetailInfo(groupId);
         return Result.success(groupDetailInfoVO);
     }
 
@@ -80,17 +66,13 @@ public class GroupController {
     @GetMapping("getGroupInfo4Chat")
     public Result<GroupInfo4ChatVO> getGroupInfo4Chat(@NotEmpty(message = Constants.VALIDATE_EMPTY_GROUP_ID) String groupId) {
         log.info("获取群聊详情(包括群成员清单) groupId: {}", groupId);
-        GroupInfo4ChatVO groupInfo4ChatVO = new GroupInfo4ChatVO();
-
         // 群聊信息
-        GroupInfo groupInfo = getBasicGroupInfo(groupId);
-        GroupDetailInfoVO groupDetailInfoVO = CopyUtils.copyBean(groupInfo, GroupDetailInfoVO.class);
-        initGroupMemberCounts(groupDetailInfoVO);
-
+        GroupDetailInfoVO groupDetailInfoVO = groupInfoService.getGroupDetailInfo(groupId);
         // 获取群成员信息
         List<BasicInfoDTO> basicInfoDTOList = userContactService.getGroupMemberInfo(groupId);
         List<BasicInfoVO> userContactList = CopyUtils.copyList(basicInfoDTOList, BasicInfoVO.class);
-
+        // 包装成vo对象
+        GroupInfo4ChatVO groupInfo4ChatVO = new GroupInfo4ChatVO();
         groupInfo4ChatVO.setGroupInfo(groupDetailInfoVO);
         groupInfo4ChatVO.setUserContactList(userContactList);
         log.info("获取群聊详情(包括群成员清单) {}", groupInfo4ChatVO);
@@ -129,44 +111,4 @@ public class GroupController {
         return Result.success();
     }
 
-    /**
-     * 给GroupInfoVO计算成员数量
-     */
-    private void initGroupMemberCounts(List<GroupDetailInfoVO> groupDetailInfoVOList) {
-        groupDetailInfoVOList.forEach(groupDetailInfoVO -> {
-            Long count = userContactService.lambdaQuery()
-                    .eq(UserContact::getContactId, groupDetailInfoVO.getGroupId())
-                    .count();
-            groupDetailInfoVO.setMemberCount(count);
-        });
-    }
-
-    private void initGroupMemberCounts(GroupDetailInfoVO groupDetailInfoVO) {
-        initGroupMemberCounts(Collections.singletonList(groupDetailInfoVO));
-    }
-
-    /**
-     * 获取群聊基本信息
-     */
-    private GroupInfo getBasicGroupInfo(String groupId) {
-        // 看该用户是否在群里
-        UserContact userContact = userContactService.lambdaQuery()
-                .eq(UserContact::getUserId, UserTokenInfoContext.getCurrentUserId())
-                .eq(UserContact::getContactId, groupId)
-                .one();
-        if (userContact == null || !UserContactStatusEnum.FRIENDS.equals(userContact.getStatus())) {
-            // 不在这个群或者被移除群了，属于非法操作
-            throw new IllegalOperationException(Constants.MESSAGE_NOT_IN_THE_GROUP);
-        }
-        GroupInfo groupInfo = groupInfoService.getById(groupId);
-        if (groupInfo == null) {
-            // 群聊不存在
-            throw new GroupNotExistException(Constants.MESSAGE_GROUP_NOT_EXIST);
-        }
-        if (GroupInfoStatusEnum.DISBAND.equals(groupInfo.getStatus())) {
-            // 群聊已解散
-            throw new GroupDisbandException(Constants.MESSAGE_GROUP_ALREADY_DISBAND);
-        }
-        return groupInfo;
-    }
 }
