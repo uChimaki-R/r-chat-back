6b60f0d62335f93c137e9a7b0dc1c8d3fa8710fc
Crestimes <1259864733@qq.com>
2024-11-09 17:20:44 +0800

    feat: 新增校验异常常量VALIDATE_EMPTY_CONTACT_NAME，值为"联系人名称不能为空"

--- src/main/java/com/r/chat/entity/constants/Constants.java
+++ src/main/java/com/r/chat/entity/constants/Constants.java
@@ -74,6 +74,7 @@ public class Constants {
     public static final String VALIDATE_EMPTY_NICKNAME = "用户名不能为空";
     public static final String VALIDATE_EMPTY_PASSWORD = "密码不能为空";
     public static final String VALIDATE_EMPTY_CONTACT_ID = "联系人id不能为空";
+    public static final String VALIDATE_EMPTY_CONTACT_NAME = "联系人名称不能为空";
     public static final String VALIDATE_EMPTY_USER_ID = "用户id不能为空";
     public static final String VALIDATE_EMPTY_GROUP_ID = "群聊id不能为空";
     public static final String VALIDATE_EMPTY_GROUP_NAME = "群聊名不能为空";
