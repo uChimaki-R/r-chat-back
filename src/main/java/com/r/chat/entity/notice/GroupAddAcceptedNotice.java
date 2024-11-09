c01cbfc26f17ea75e4163812164ac31ff50c8c0b
Crestimes <1259864733@qq.com>
2024-11-09 10:50:54 +0800

    refactor: 使用ChatMessageVO类属性代替原来的ChatSessionUserVO类属性保存需要返回给前端的数据。

--- src/main/java/com/r/chat/entity/notice/GroupAddAcceptedNotice.java
+++ src/main/java/com/r/chat/entity/notice/GroupAddAcceptedNotice.java
@@ -2,7 +2,7 @@ package com.r.chat.entity.notice;
 
 import com.r.chat.entity.enums.NoticeTypeEnum;
 import com.r.chat.entity.result.Notice;
-import com.r.chat.entity.vo.ChatSessionUserVO;
+import com.r.chat.entity.vo.ChatMessageVO;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -23,5 +23,5 @@ public class GroupAddAcceptedNotice extends Notice implements Serializable {
     /**
      * 用于渲染会话框的内容，lastMessage就是自己加入群聊的提示信息
      */
-    private ChatSessionUserVO chatSessionUserVO;
+    private ChatMessageVO chatMessageVO;
 }
