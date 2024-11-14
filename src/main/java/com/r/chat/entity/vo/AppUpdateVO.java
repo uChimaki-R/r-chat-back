d91192d57d494b8a8c84f4df3276aa01bec6cff9
Crestimes <1259864733@qq.com>
2024-11-14 11:35:03 +0800

    feat: 应用更新VO类补充description、descriptionList、createTime、releaseTime、status、grayscaleIds、grayscaleIdList属性，用于前端的展示。

--- src/main/java/com/r/chat/entity/vo/AppUpdateVO.java
+++ src/main/java/com/r/chat/entity/vo/AppUpdateVO.java
@@ -1,11 +1,15 @@
 package com.r.chat.entity.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.r.chat.entity.enums.AppUpdateMethodTypeEnum;
+import com.r.chat.entity.enums.AppUpdateStatusEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -25,9 +29,43 @@ public class AppUpdateVO implements Serializable {
     private String version;
 
     /**
+     * 更新描述
+     */
+    private String description;
+
+    /**
      * 更新描述转换的列表
      */
-    private List<String> updateList;
+    private List<String> descriptionList;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
+
+    /**
+     * 发布时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime releaseTime;
+
+    /**
+     * 发布状态 0：未发布 1：灰度发布 2：全网发布
+     */
+    private AppUpdateStatusEnum status;
+
+    /**
+     * 灰度发布的用户id（使用逗号分割）
+     */
+    private String grayscaleIds;
+
+    /**
+     * 灰度发布的用户id组成的列表
+     */
+    private List<String> grayscaleIdList;
 
     /**
      * 更新手段 0：文件 1：外链
