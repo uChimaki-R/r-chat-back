651c15dcd535f85184a1155ecfadf6a7f5afdeb5
Crestimes <1259864733@qq.com>
2024-11-14 11:33:32 +0800

    refactor: 使用Date类代替LocalDateTime类接收前端传入的日期数据。删除部分冗余代码。

--- src/main/java/com/r/chat/entity/dto/AppUpdateQueryDTO.java
+++ src/main/java/com/r/chat/entity/dto/AppUpdateQueryDTO.java
@@ -1,13 +1,12 @@
 package com.r.chat.entity.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -15,11 +14,9 @@ import java.time.LocalDateTime;
 public class AppUpdateQueryDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime startTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date startTime;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime endTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date endTime;
 }
