1b4449675a2fde502406bf6287a319dd2fe40e20
Crestimes <1259864733@qq.com>
2025-02-25 10:12:08 +0800

    fix: 将所有String类转换器的默认编码修改为UTF-8，解决controller接口返回Flux类时中文显示问号的问题。 feat: 配置异步线程池以使用Flux返回数据。 refactor: 删除TokenInterceptor拦截器对swagger相关路径的排除配置。 refactor: 删除对静态资源映射方法的重写（swagger配置）。

--- src/main/java/com/r/chat/config/WebMvcConfig.java
+++ src/main/java/com/r/chat/config/WebMvcConfig.java
@@ -6,10 +6,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.context.request.async.TimeoutCallableProcessingInterceptor;
 import org.springframework.web.filter.FormContentFilter;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -35,7 +42,6 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("注册自定义拦截器 TokenInterceptor");
         registry.addInterceptor(tokenInterceptor)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/swagger-resources/**","/swagger-ui/**", "/v3/**", "/error")  // NoHandlerFoundException映射到的error请求、knife4j的请求
                 .excludePathPatterns("/user/checkCode")  // 验证码
                 .excludePathPatterns("/user/register")  // 注册
                 .excludePathPatterns("/user/login");  // 登录
@@ -45,15 +51,43 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     }
 
     /**
-     * 设置静态资源映射
+     * Ollama的controller接口返回Flux类时中文会变成问号，这里将所有String类转换器的默认编码修改为UTF-8以解决这个问题
      */
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html", "doc.html", "favicon.ico").addResourceLocations(
-                "classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations(
-                "classpath:/META-INF/resources/webjars/");
-        super.addResourceHandlers(registry);
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.stream()
+                // 过滤出StringHttpMessageConverter类型实例
+                .filter(StringHttpMessageConverter.class::isInstance)
+                .map(c -> (StringHttpMessageConverter) c)
+                // 这里将转换器的默认编码设置为utf-8
+                .forEach(c -> c.setDefaultCharset(StandardCharsets.UTF_8));
     }
 
+    /**
+     * Performing asynchronous handling through the default Spring MVC SimpleAsyncTaskExecutor.
+     * This executor is not suitable for production use under load.
+     * Please, configure an AsyncTaskExecutor through the WebMvc config.<br>
+     * 使用了Flux返回数据，需要配置异步线程池
+     */
+    @Override
+    public void configureAsyncSupport(final AsyncSupportConfigurer configurer) {
+        configurer.setDefaultTimeout(60 * 1000L);
+        configurer.registerCallableInterceptors(timeoutInterceptor());
+        configurer.setTaskExecutor(threadPoolTaskExecutor());
+    }
+
+    @Bean
+    public TimeoutCallableProcessingInterceptor timeoutInterceptor() {
+        return new TimeoutCallableProcessingInterceptor();
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor t = new ThreadPoolTaskExecutor();
+        t.setCorePoolSize(10);
+        t.setMaxPoolSize(50);
+        t.setQueueCapacity(10);
+        t.setThreadNamePrefix("WEB-Thread-");
+        return t;
+    }
 }
