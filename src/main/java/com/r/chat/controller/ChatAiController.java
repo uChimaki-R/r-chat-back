e4baeb72395d2331ce5291891c8be12a26da6294
Ray-C <1259864733@qq.com>
2025-03-31 15:17:57 +0800

    refactor: 添加mode请求参数指明ai调用类型，添加和修改相关代码逻辑。

--- src/main/java/com/r/chat/controller/ChatAiController.java
+++ src/main/java/com/r/chat/controller/ChatAiController.java
@@ -1,13 +1,14 @@
 package com.r.chat.controller;
 
+import com.r.chat.context.UserTokenInfoContext;
+import com.r.chat.entity.constants.Constants;
+import com.r.chat.entity.enums.AiModeEnum;
+import com.r.chat.exception.ParameterErrorException;
 import com.r.chat.utils.ChatAiUtils;
 import com.r.chat.utils.SseEmitterUTF8;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import javax.annotation.Resource;
@@ -28,7 +29,12 @@ public class ChatAiController {
     private ChatAiUtils chatAiUtils;
 
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter chat(String message, HttpServletResponse response) {
+    public SseEmitter chat(@RequestParam String message, @RequestParam Integer mode, HttpServletResponse response) {
+        AiModeEnum aiModeEnum = AiModeEnum.getAiModeEnum(mode);
+        if (aiModeEnum == null) {
+            throw new ParameterErrorException(Constants.MESSAGE_ENUM_ERROR);
+        }
+        log.info("Ai chat | sessionId: {}, message: {}, mode: {}", UserTokenInfoContext.getCurrentUserId(), message, aiModeEnum);
         // 设置流式响应头
         response.setContentType("text/event-stream");
         response.setCharacterEncoding("UTF-8");
@@ -36,7 +42,7 @@ public class ChatAiController {
         // 使用自定义的utf-8编码的SseEmitter
         SseEmitter sseEmitter = new SseEmitterUTF8();
         // 发送请求，配置SseEmitter流式返回
-        chatAiUtils.configEmitterAndGetAnswer(sseEmitter, "user-test", message);
+        chatAiUtils.configEmitterAndGetAnswer(sseEmitter, UserTokenInfoContext.getCurrentUserId(), message, aiModeEnum);
         return sseEmitter;
     }
 }
